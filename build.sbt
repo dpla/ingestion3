85437b04ff1c38dce39c41284d400b0f0d304476
Scott Williams <scott@dp.la>
2017-01-18 11:23:24 -0500

    Add .idea/ to .gitignore Add ApiHarvest dependencie to build.sbt Add ApiHarvester from Scetta CDL tests

--- build.sbt
+++ build.sbt
@@ -14,5 +14,11 @@ libraryDependencies ++= Seq(
   "org.eclipse.rdf4j" % "rdf4j" % "2.1.4",
   "org.elasticsearch" % "elasticsearch-hadoop" % "2.0.3"
     excludeAll(ExclusionRule("cascading", "cascading-hadoop"), ExclusionRule("cascading", "cascading-local")),
-  "org.scalatest" %% "scalatest" % "3.0.0" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.0" % "test",
+  //  ApiHarvester depends
+  "org.apache.httpcomponents" % "httpclient" % "4.5.2",
+  "com.lambdaworks" %% "jacks" % "2.3.3",
+//  Needed for SequenceFile writing
+  "org.apache.hadoop" % "hadoop-mapreduce-client-jobclient" % "2.7.3",
+  "org.apache.hadoop" % "hadoop-common" % "2.7.3"
 )
---
+++ src/main/scala/la/dp/ingestion3/harvesters/ApiHarvester.scala
@@ -0,0 +1,84 @@
+
+package la.dp.ingestion3
+
+import org.apache.http.HttpEntity
+import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet}
+import org.apache.http.client.utils.URIBuilder
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.util.EntityUtils
+import org.json4s.jackson.JsonMethods._
+import org.apache.commons.codec.digest.DigestUtils
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.io.SequenceFile.Writer
+
+trait ApiHarvester {
+  
+  /**
+    * Prototype for API harvester
+    *
+   */
+  def harvest(endpoint: String,
+              query: String="*:*",
+              key: String,
+              offset: String="0",
+              fetchSize: String="10",
+              outDir: String,
+              writer: Writer): Int = {
+
+    var fetchAgain = false
+    val httpclient = HttpClients.createDefault()
+    // Construct the query URI
+    val uri = new URIBuilder()
+      .setScheme("https")
+      .setHost(endpoint)
+      .setPath("/solr/query")
+      .setParameter("q", query)
+      .setParameter("start", offset)
+      .setParameter("rows", fetchSize)
+      .build()
+
+    val httpget = new HttpGet(uri)
+
+    // Set the authentication token
+    httpget.addHeader("X-Authentication-Token", key)
+    // Execute the GET request
+    val response: CloseableHttpResponse = httpclient.execute(httpget)
+
+    try {
+      val entity: HttpEntity = response.getEntity()
+
+      if (entity != null) {
+        // Parse the JSON
+        val json = parse( EntityUtils.toString(entity) )
+        // Select the docs array
+        val docs = (json \\ "docs")
+        // If the number of docs returned is less than the number
+        // requested then there is no more to fetch
+        fetchAgain = docs.children.length == fetchSize.toInt
+
+        for (doc <- (json \\ "docs").children) {
+          // Generate the DPLA id
+          val identifier: String = generateMd5((doc \ "id").toString)
+          // Write to Hadoop Seq File
+          writer.append(new Text(identifier), new Text(compact(doc)))
+        }
+      }
+    } finally {
+      response.close()
+    }
+
+    if (fetchAgain) {
+      return offset.toInt + fetchSize.toInt
+    }
+    return -1
+  }
+
+  /**
+    * This one liner taken from:
+    * http://stackoverflow.com/questions/38855843/scala-one-liner-to-generate-md5-hash-from-string
+    * MessageDigest.getInstance("MD5").digest(id.getBytes()).map(0xFF & _).map { "%02x".format(_) }.foldLeft("") {_ + _}
+    */
+  def generateMd5(id: String): String = {
+    DigestUtils.md5Hex(id)
+  }
+}
