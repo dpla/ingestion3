5d32c10fba3e98aaf1b4ace7f5da636e080e90cb
Scott Williams <scott@dp.la>
2017-01-28 00:24:19 -0500

    Remove print stmt Add comments and description

--- src/main/scala/la/dp/ingestion3/harvesters/Harvester.scala
+++ src/main/scala/la/dp/ingestion3/harvesters/Harvester.scala
@@ -10,12 +10,15 @@ object Harvester {
     * The only shared method between harvester implementations. Generates
     * an md5 hash of the id value
     *
-    * @param id String to hash
+    * @param id String
+    *           Provider's record id to hash
+    * @param prov String
+    *             Optional, Provider abbreviation if provided is prepended to
+    *             id and then hashed
     * @return String md5 hash of the id value
     */
   def generateMd5(id: String, prov: String=""): String = {
     if(prov.nonEmpty)
-      println( List(prov,id).mkString("--").trim )
       DigestUtils.md5Hex(List(prov,id).mkString("--").trim)
     DigestUtils.md5Hex(id)
   }
