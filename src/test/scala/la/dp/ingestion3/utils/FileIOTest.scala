dc5eafb620d84ce225cc327e98b72d8b04e5b030
Scott Williams <scott@dp.la>
2017-01-31 00:43:59 -0500

    Add FileIO tests

---
+++ src/test/scala/la/dp/ingestion3/utils/FileIOTest.scala
@@ -0,0 +1,28 @@
+package la.dp.ingestion3.utils
+
+import java.io.File
+
+import org.scalatest.{FlatSpec, Matchers}
+
+
+/**
+  * Created by @Scott on 1/30/17.
+  */
+class FileIOTest extends FlatSpec with Matchers {
+  "writeFile " should " create a file" in {
+    val f = new File("./file.txt")
+    FileIO.writeFile("Test", new java.io.File("./file.txt"))
+    assert(f.exists)
+    f.delete() // cleanup
+  }
+
+  "writeFiles " should " create two files" in {
+    val files = Map[java.io.File,String]( new File("./file_1.txt") -> "Test one.",
+                                          new File("./file_2.txt") -> "Test two")
+    FileIO.writeFiles(files)
+    files.foreach( f => {
+      assert(f._1.exists)
+      f._1.delete() // cleanup
+    } )
+  }
+}
