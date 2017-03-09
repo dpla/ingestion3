7c504ec46bb9a0d3f641623fd5460f2a66ed710d
Scott Williams <scott@dp.la>
2017-03-08 21:46:13 -0800

    Add base case for getRecordsAsMap

--- src/main/scala/la/dp/ingestion3/harvesters/OaiResponseProcessor.scala
+++ src/main/scala/la/dp/ingestion3/harvesters/OaiResponseProcessor.scala
@@ -43,7 +43,8 @@ class OaiResponseProcessor() {
         val localId = Harvester.getLocalId(node)
         (Harvester.generateMd5(localId), node.toString)
       }
-    }) //.toMap
+      case _ => ("", "")
+    })
   }
 
   /**
