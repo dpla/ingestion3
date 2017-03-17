4534db1bbc8b12d40ca1ada4723b751bd082359e
Scott Williams <scott@dp.la>
2017-03-17 10:49:54 -0400

    Add ResourceSyncRdd

--- src/main/scala/dpla/ingestion3/utils/ResourceSyncRdd.scala
+++ src/main/scala/dpla/ingestion3/utils/ResourceSyncRdd.scala
@@ -1,9 +1,46 @@
 package dpla.ingestion3.utils
 
-import org.apache.spark.Partition
+import dpla.ingestion3.harvesters.ResourceSyncUrlBuilder
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{Partition, SparkContext, TaskContext}
 
-class ResourceSyncPartition(rddId: Int, idx: Int, val resourceListUrl: String) extends Partition {}
+import scala.xml._
+import scalaj.http.Http
 
-class RsRdd {
 
+class ResourceSyncPartition(val resourceUrl: String) extends Partition {
+  override def index: Int = 0
+}
+
+class ResourceSyncRdd (resourceListUrl: String, sc: SparkContext) extends RDD[String](sc, Nil) {
+
+  override protected def getPartitions: Array[ResourceSyncPartition] = {
+    val rsp = XML.load(resourceListUrl)
+    val resourceUrls = rsp \\ "url" \\ "loc"
+    resourceUrls.map(r => new ResourceSyncPartition(r.text)).toArray
+  }
+
+  override def compute(part: Partition, context: TaskContext): Iterator[String] = {
+    val p = part.asInstanceOf[ResourceSyncPartition]
+    val resourceUrl = p.resourceUrl
+
+    new Iterator[String] {
+      var nextURL = ""
+      override def hasNext: Boolean = {
+        nextURL = resourceUrl
+        resourceUrl.nonEmpty
+      }
+
+      /**
+        * this needs some more thought but should be functional
+        * @return
+        */
+      override def next(): String = {
+        val urlBuilder = new ResourceSyncUrlBuilder()
+        val url = urlBuilder.buildQueryUrl(Map("endpoint"->nextURL))
+
+        Http(url.toString).toString
+      }
+    }
+  }
 }
