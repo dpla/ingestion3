dcb68b71c26a136ca18eb600b3932838fea718cb
Scott Williams <scott@dp.la>
2017-05-22 15:57:24 -0400

    Inital stub for string enrichments

---
+++ src/main/scala/dpla/ingestion3/enrichments/StringEnrichments.scala
@@ -0,0 +1,12 @@
+package dpla.ingestion3.enrichments
+
+/**
+  * String enrichments
+  */
+class StringEnrichments {
+  def convertToSentenceCase(value: String): String = ???
+
+  def splitAtDelimiter(value: String): String = ???
+
+  def splitOnProvidedLabel(value: String): String = ???
+}
