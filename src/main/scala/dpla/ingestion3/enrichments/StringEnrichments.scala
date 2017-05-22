9fa15ad454714c0f1fc0d60905db5f37ef093346
Scott Williams <scott@dp.la>
2017-05-22 16:09:57 -0400

    Finish stub out

--- src/main/scala/dpla/ingestion3/enrichments/StringEnrichments.scala
+++ src/main/scala/dpla/ingestion3/enrichments/StringEnrichments.scala
@@ -6,7 +6,19 @@ package dpla.ingestion3.enrichments
 class StringEnrichments {
   def convertToSentenceCase(value: String): String = ???
 
+  def limitCharacters(value: String): String = ???
+
   def splitAtDelimiter(value: String): String = ???
 
   def splitOnProvidedLabel(value: String): String = ???
+
+  def splitProvidedLabelAtDelimiter(value: String): String = ???
+
+  def stripEndingPunctuation(value: String): String = ???
+
+  def stripHTML(value: String): String = ???
+
+  def stripLeadingPunctuation(value: String): String = ???
+
+  def stripPunctuation(value: String): String = ???
 }
