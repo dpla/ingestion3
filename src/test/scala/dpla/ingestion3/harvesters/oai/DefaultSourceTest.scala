3d97dc05e30555d89d3199440c85c2119640212d
Audrey Altman <audrey@dp.la>
2017-07-20 15:32:10 -0400

    add unit tests for OAI harvester

---
+++ src/test/scala/dpla/ingestion3/harvesters/oai/DefaultSourceTest.scala
@@ -0,0 +1,74 @@
+package dpla.ingestion3.harvesters.oai
+
+import org.scalatest._
+
+/**
+  * Tests for DefaultSourceTest
+  */
+class DefaultSourceTest extends FlatSpec {
+  val source = new DefaultSource
+  val endpoint = "http://repox.example.edu:8080/repox/OAIHandler"
+  val verb = "ListRecords"
+  val list = "manuscripts, newspapers, maps"
+  val prefix = "mods"
+  val token = "1234"
+
+  "getEndpoint" should "throw Exception if endpoint missing" in {
+    val params = Map("foo" -> "bar")
+    assertThrows[Exception](source.getEndpoint(params))
+  }
+  it should "return endpoint as String" in {
+    val params = Map("path" -> endpoint)
+    assert(source.getEndpoint(params) === endpoint)
+  }
+
+  "getVerb" should "throw Exception if verb missing" in {
+    val params = Map("foo" -> "bar")
+    assertThrows[Exception](source.getVerb(params))
+  }
+  it should "return endpoint as String" in {
+    val params = Map("verb" -> verb)
+    assert(source.getVerb(params) === verb)
+  }
+
+  "getHarvestAllRecords" should "return false if harvestAllSets missing" in {
+    val params = Map("foo" -> "bar")
+    assert(source.getHarvestAllSets(params) === false)
+  }
+  it should "return true if harvestAllSets is true" in {
+    val params = Map("harvestAllSets" -> "true")
+    assert(source.getHarvestAllSets(params) === true)
+  }
+  it should "return false if harvestAllSets is false" in {
+    val params = Map("harvestAllSets" -> "false")
+    assert(source.getHarvestAllSets(params) === false)
+  }
+  it should "ignore case" in {
+    val params = Map("harvestAllSets" -> "TRUE")
+    assert(source.getHarvestAllSets(params) === true)
+  }
+  it should "throw Exception if harvestAllSets is neither true nor false" in {
+    val params = Map("harvestAllSets" -> "foo")
+    assertThrows[Exception](source.getHarvestAllSets(params))
+  }
+
+  "getSetlist" should "return None if setlist missing" in {
+    val params = Map("foo" -> "bar")
+    assert(source.getSetlist(params) === None)
+  }
+  it should "return set list as Some[Array[String]]" in {
+    val params = Map("setlist" -> list)
+    val sourceOption = source.getSetlist(params).getOrElse(Array())
+    assert(sourceOption.deep === Array("manuscripts", "newspapers", "maps").deep)
+  }
+
+  "getBlacklist" should "return None if blacklist missing" in {
+    val params = Map("foo" -> "bar")
+    assert(source.getBlacklist(params) === None)
+  }
+  it should "return set list as Some[Array[String]]" in {
+    val params = Map("blacklist" -> list)
+    val sourceOption = source.getBlacklist(params).getOrElse(Array())
+    assert(sourceOption.deep === Array("manuscripts", "newspapers", "maps").deep)
+  }
+}
--- src/test/scala/dpla/ingestion3/harvesters/oai/OaiResponseProcessorTest.scala
+++ src/test/scala/dpla/ingestion3/harvesters/oai/OaiResponseProcessorTest.scala
@@ -1,45 +1,66 @@
 package dpla.ingestion3.harvesters.oai
 
 import org.scalatest._
-
+import org.scalamock.scalatest.MockFactory
 
 /**
   * Tests for OaiResponseProcessor
-  *
-  * TODO: Figure out how to simulate http requests/responses.
-  *
   */
-class OaiResponseProcessorTest extends FlatSpec with Matchers with BeforeAndAfter {
+class OaiResponseProcessorTest extends FlatSpec with MockFactory {
 
-//  val validOaiXml = dpla.ingestion3.data.TestOaiData.paOaiListRecordsRsp
-//  val invalidOaiXml = dpla.ingestion3.data.TestOaiData.paOaiErrorRsp
-//
-//  val outDir = new File("/dev/null")
-//  val oaiUrl = new java.net.URL("http://aggregator.padigital.org/oai")
-//  val oaiVerb = "ListRecords"
-//  val prefix = "oai_dc"
-//  val fileIO = new FlatFileIO
-//  val urlBuilder = new OaiQueryUrlBuilder
-//  val harvester = new OaiResponseProcessor()
-//
-//  "getOaiErrorCode " should " return Option.None if there is no error code" in {
-//    harvester.getOaiErrorCode(validOaiXml) shouldBe None
-//  }
-//
-//  it should " throw a HarvesterException if there is an error code " in {
-//    assertThrows[HarvesterException] {
-//      harvester.getOaiErrorCode(invalidOaiXml)
-//    }
-//  }
-//
-//  "getResumptionToken() " should " return None if there is an error in the response " in {
-//    assert(harvester.getResumptionToken(invalidOaiXml) === None)
-//  }
-//
-//  it should " return a non-empty String value if the response is valid and incomplete" in {
-//    assert(harvester.getResumptionToken(validOaiXml).get.nonEmpty)
-//  }
-//  it should " equal '90d421891feba6922f57a59868d7bcd1'" in {
-//    assert(harvester.getResumptionToken(validOaiXml).get === "90d421891feba6922f57a59868d7bcd1")
-//  }
+  val validRecordOaiXml = dpla.ingestion3.data.TestOaiData.paOaiListRecordsRsp
+  val validSetOaiXml = dpla.ingestion3.data.TestOaiData.inOaiListSetsRsp
+  val errorOaiXml = dpla.ingestion3.data.TestOaiData.paOaiErrorRsp
+
+  "getRecords" should "return the correct number of records" in {
+    val records = OaiResponseProcessor.getRecords(validRecordOaiXml)
+    assert(records.size === 10)
+  }
+  it should "add OaiSet to OaiRecord if OaiSet is given" in {
+    val mockOaiSet = mock[OaiSet]
+    val records = OaiResponseProcessor.getRecords(validRecordOaiXml, Some(mockOaiSet))
+    assert(records(0).set.nonEmpty)
+  }
+  it should "return empty sequence if XML page contains no records" in {
+    val records = OaiResponseProcessor.getRecords(validSetOaiXml)
+    assert(records.size === 0)
+  }
+  it should "parse record id" in {
+    val expectedId = "oai:libcollab.temple.edu:fedora-system:ContentModel-3.0"
+    val records = OaiResponseProcessor.getRecords(validRecordOaiXml)
+    // Use contains instead of === to handle whitespace in test data.
+    assert(records(0).id.contains(expectedId))
+  }
+
+  "getSets" should "return the correct number of sets" in {
+    val sets = OaiResponseProcessor.getSets(validSetOaiXml)
+    assert(sets.size === 5)
+  }
+  it should "return empty sequence if XML page contains no sets" in {
+    val sets = OaiResponseProcessor.getSets(validRecordOaiXml)
+    assert(sets.size === 0)
+  }
+  it should "parse set id" in {
+    val expectedId = "PALNI_winona"
+    val sets = OaiResponseProcessor.getSets(validSetOaiXml)
+    assert(sets(0).id === expectedId)
+  }
+
+  "getResumptionToken" should "return the resumption token" in {
+    val expectedToken = "90d421891feba6922f57a59868d7bcd1"
+    val token = OaiResponseProcessor.getResumptionToken(validRecordOaiXml.toString)
+    assert(token === Some(expectedToken))
+  }
+  it should "return None in absence of resumption token" in {
+    val token = OaiResponseProcessor.getResumptionToken(validSetOaiXml.toString)
+    assert(token === None)
+  }
+
+  "getOaiErrorCode" should "return Unit if there is no error code" in {
+    val error = OaiResponseProcessor.getOaiErrorCode(validRecordOaiXml)
+    assert(error.isInstanceOf[Unit])
+  }
+  it should "throw an Exception if there is an error code" in {
+    assertThrows[Exception](OaiResponseProcessor.getOaiErrorCode(errorOaiXml))
+  }
 }
