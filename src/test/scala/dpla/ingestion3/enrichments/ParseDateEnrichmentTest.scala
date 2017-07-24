9f1f156f059f1840c538105bf02d02443c38356a
scott <scott.brandon.williams@gmail.com>
2017-07-24 11:53:58 -0400

    Initial work on enrichment driver (#40)
    * Initial work on enrichment driver. Required changes to lexvo, date and type enrichments may be included in this branch.
    * Added tests for lexvo enrichment
    * Removed ISO-639 vocab case class
    * Refactor date enrichment to accept EdmTimeSpan rather than String
    * Added tests for EnrichmentDriver

--- src/test/scala/dpla/ingestion3/enrichments/ParseDateEnrichmentTest.scala
+++ src/test/scala/dpla/ingestion3/enrichments/ParseDateEnrichmentTest.scala
@@ -1,31 +1,104 @@
 package dpla.ingestion3.enrichments
 
+import dpla.ingestion3.model.EdmTimeSpan
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 
 class ParseDateEnrichmentTest extends FlatSpec with BeforeAndAfter {
   val enrichment = new ParseDateEnrichment
 
-  "ParseDateEnrichment" should "parse calendar to iso Date'" in
-    assert(enrichment.parse("May 15, 2014") === Some("2014-05-15"))
+  "ParseDateEnrichment" should "parse calendar to iso Date'" in {
+    val date = "May 15, 2014"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = Some("2014-05-15"),
+      begin = None,
+      end = None
+    )
 
-  it should "parse iso to iso Date" in
-    assert(enrichment.parse("2014-05-15") === Some("2014-05-15"))
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
 
-  it should "parse slash date to iso Date" in
-    assert(enrichment.parse("5/7/2012") === Some("2012-05-07"))
 
-  it should "parse dot date to iso date" in
-    assert(enrichment.parse("5.7.2012") === Some("2012-05-07"))
+  it should "parse iso to iso Date" in {
+    val date = "2014-05-15"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = Some("2014-05-15"),
+      begin = None,
+      end = None
+    )
 
-  it should "parse Month, Year to iso date" in
-    assert(enrichment.parse("July, 2015") === Some("2015-07-01"))
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
 
-  it should "parse M-D-Y to iso date" in
-    assert(enrichment.parse("12-19-2010") === Some("2010-12-19"))
 
-  ignore should "parses uncertain to EDTF" in //todo edtf
-    assert(enrichment.parse("2015?") === Some("2015?"))
+  it should "parse slash date to iso Date" in {
+    val date = "5/7/2012"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = Some("2012-05-07"),
+      begin = None,
+      end = None
+    )
 
-  it should "returns None for unparsable dates" in
-    assert(enrichment.parse("emefragramoofabits") === None)
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
+
+  it should "parse dot date to iso date" in {
+    val date = "5.7.2012"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = Some("2012-05-07"),
+      begin = None,
+      end = None
+    )
+
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
+
+  it should "parse Month, Year to iso date" in {
+    val date = "July, 2015"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = Some("2015-07-01"),
+      begin = None,
+      end = None
+    )
+
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
+
+  it should "parse M-D-Y to iso date" in {
+    val date = "12-19-2010"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = Some("2010-12-19"),
+      begin = None,
+      end = None
+    )
+
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
+
+//  ignore should "parses uncertain to EDTF" in //todo edtf
+//    assert(enrichment.parse("2015?") === Some("2015?"))
+
+  it should "returns None for unparsable dates" in {
+    val date = "emefragramoofabits"
+    val originalDate = EdmTimeSpan(originalSourceDate = Some(date))
+    val enrichedDate = EdmTimeSpan(
+      originalSourceDate = Some(date),
+      prefLabel = None,
+      begin = None,
+      end = None
+    )
+
+    assert(enrichment.parse(originalDate) === enrichedDate)
+  }
 }
