dfc39f96af398d668b54d0de92b4de5e2967b5a6
Scott Williams <scott@dp.la>
2017-07-25 09:25:32 -0400

    * Inital work on generalized ID and URI minting methods and integrating base provider ID selection into mapping/extraction for CDL and PA. * Add 'id' and '@id' properties to DplaMapData case class

--- src/main/scala/dpla/ingestion3/mappers/providers/Extractor.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/Extractor.scala
@@ -1,12 +1,40 @@
 package dpla.ingestion3.mappers.providers
 
+import java.net.URI
+
 import dpla.ingestion3.model.{DplaMapData, EdmAgent}
+import dpla.ingestion3.utils.Utils
+import org.apache.pig.data.utils.MethodHelper.NotImplemented
 
 /**
   * Interface that all provider extractors implement.
   */
 
 trait Extractor {
-  def build(rawData: String): DplaMapData
+  // Base item uri
+  private val baseItemUri = "http://dp.la/api/items/"
+
+  def build(): DplaMapData
   def agent: EdmAgent
+  /**
+    * Build the base ID to be hashed. Implemented per provider
+    *
+    * @return String
+    * @throws NotImplemented
+    */
+  def getProviderBaseId(): Option[String]
+
+  /**
+    * Hashes the base ID
+    *
+    * @return MD5 hash of the base ID
+    */
+  protected def mintDplaId(): String = Utils.generateMd5(getProviderBaseId())
+
+  /**
+    * Builds the item URI
+    *
+    * @return URI
+    */
+  protected def mintDplaItemUri(): URI = new URI(s"${baseItemUri}${mintDplaId()}")
 }
