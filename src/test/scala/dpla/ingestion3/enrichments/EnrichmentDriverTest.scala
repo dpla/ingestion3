450de6a8e3c049e87a7bf306e19cf333dd56112b
Michael Della Bitta <mdellabitta@gmail.com>
2017-07-27 16:28:53 -0400

    Fixed nondeterministic execution (#48)
    Changed the way fallthrough execution works in parse() to be cleaner
    and to avoid race conditions.
    
    Fixed potential race conditions from shared SimpleDateFormats.

--- src/test/scala/dpla/ingestion3/enrichments/EnrichmentDriverTest.scala
+++ src/test/scala/dpla/ingestion3/enrichments/EnrichmentDriverTest.scala
@@ -2,28 +2,55 @@ package dpla.ingestion3.enrichments
 
 import java.net.URI
 
-import dpla.ingestion3.data.MappedRecordsTest
-import dpla.ingestion3.model.{DplaSourceResource, EdmTimeSpan, SkosConcept}
+
+import dpla.ingestion3.model._
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 
 class EnrichmentDriverTest extends FlatSpec with BeforeAndAfter {
 
   val driver = new EnrichmentDriver
 
+  val mappedRecord = DplaMapData(
+    DplaSourceResource(
+      date = Seq(EdmTimeSpan(
+        originalSourceDate=Some("4.3.2015"),
+        prefLabel = None,
+        begin = None,
+        end = None
+      )),
+      language = Seq(SkosConcept(
+        providedLabel = Some("eng"),
+        note = None,
+        scheme = None,
+        exactMatch= Seq(),
+        closeMatch = Seq())
+      )
+    ),
+    EdmWebResource(
+      uri = new URI("")
+    ),
+    OreAggregation(
+      dataProvider = EdmAgent(),
+      uri = new URI(""), //uri of the record on our site
+      originalRecord = "", //map v4 specifies this as a ref, but that's LDP maybe?
+      provider = EdmAgent()
+    )
+  )
+
   "EnrichmentDriver" should " enrich both language and date" in {
-    val expectedValue = MappedRecordsTest.mappedRecord.copy(new DplaSourceResource(
-      date = Seq(new EdmTimeSpan(
-          prefLabel = Some("2012-05-07"),
-          originalSourceDate = Some("5.7.2012")
+    val expectedValue = mappedRecord.copy(DplaSourceResource(
+      date = Seq(EdmTimeSpan(
+          prefLabel = Some("2015-04-03"),
+          originalSourceDate = Some("4.3.2015")
         )),
       language = Seq(SkosConcept(
-        providedLabel = Option("eng"),
-        concept = Option("English"),
-        scheme = Option(new URI("http://lexvo.org/id/iso639-3/")))
+        providedLabel = Some("eng"),
+        concept = Some("English"),
+        scheme = Some(new URI("http://lexvo.org/id/iso639-3/")))
       )
     ))
 
-    val enrichedRecord = driver.enrich(MappedRecordsTest.mappedRecord)
+    val enrichedRecord = driver.enrich(mappedRecord)
 
     assert(enrichedRecord === expectedValue)
   }
