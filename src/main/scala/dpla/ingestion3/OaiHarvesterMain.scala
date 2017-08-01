9ce1640f87711313df2e9e359e6756bbaeaf8388
Scott Williams <scott@dp.la>
2017-08-01 12:27:53 -0400

    Fixup OaiHarvesterMain

--- src/main/scala/dpla/ingestion3/OaiHarvesterMain.scala
+++ src/main/scala/dpla/ingestion3/OaiHarvesterMain.scala
@@ -2,59 +2,68 @@ package dpla.ingestion3
 
 import java.io.File
 
-import dpla.ingestion3.utils.{FlatFileIO, Utils}
+import dpla.ingestion3.utils.Utils
 import com.databricks.spark.avro._
 import dpla.ingestion3.confs.OaiHarvesterConf
 import org.apache.log4j.LogManager
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.storage.StorageLevel
 
+import scala.util.{Failure, Success, Try}
 
 /**
   * Entry point for running an OAI harvest.
   */
 object OaiHarvesterMain {
-  
+
   val logger = LogManager.getLogger(OaiHarvesterMain.getClass)
 
   def main(args: Array[String]): Unit = {
     val oaiConf = new OaiHarvesterConf(args.toSeq)
+    val oaiParams = oaiConf.load()
 
-    Utils.deleteRecursively(new File(oaiConf.outputDir()))
+    // TODO print something pleasant.
+    Utils.deleteRecursively(new File(oaiParams.outputDir.get))
 
     // Initiate spark session.
     val sparkConf = new SparkConf().setAppName("Oai Harvest")
     // sparkMaster has a default value of local[*] if not provided.
     // TODO: will this default value work with EMR?
-    sparkConf.setMaster(oaiConf.sparkMaster())
+    sparkConf.setMaster(oaiParams.sparkMaster.get)
 
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
     val sc = spark.sparkContext
-    val schema = new FlatFileIO().readFileAsString("/avro/OriginalRecord.avsc")
 
-    val start = System.currentTimeMillis()
-
+    // Set options
     val readerOptions: Map[String, String] = Map(
-      "verb" -> oaiConf.verb.toOption,
-      "metadataPrefix" -> oaiConf.prefix.toOption,
-      "harvestAllSets" -> oaiConf.harvestAllSets.toOption,
-      "setlist" -> oaiConf.setlist.toOption,
-      "blacklist" -> oaiConf.blacklist.toOption
+      "verb" -> oaiParams.verb,
+      "metadataPrefix" -> oaiParams.metadataPrefix,
+      "harvestAllSets" -> oaiParams.harvestAllSets,
+      "setlist" -> oaiParams.setList,
+      "blacklist" -> oaiParams.blacklist,
+      "endpoint" -> oaiParams.endpoint
     ).collect{ case (key, Some(value)) => key -> value } // remove None values
 
-    val results = spark.read
-      .format("dpla.ingestion3.harvesters.oai")
-      .options(readerOptions)
-      .load(oaiConf.endpoint())
+    // These were already validated in OaiHarvesterConf so this is redundant but did not want
+    // to call .get() on an option to access the properties when saving the avro
+    val outputDir = oaiParams.outputDir match {
+      case Some(d) => d
+      case _ => throw new IllegalArgumentException("Output directory is not specified. Terminating run.")
+    }
 
-    results.persist(StorageLevel.DISK_ONLY)
+    val provider = oaiParams.provider match {
+      case Some(p) => p
+      case _ => throw new IllegalArgumentException("Provider is not specified. Terminating run.")
+    }
 
-    val dataframe = results.withColumn("provider", lit(oaiConf.provider()))
-      .withColumn("mimetype", lit("application_xml"))
-
-    val recordsHarvestedCount = dataframe.count()
+    def runHarvest(): Try[DataFrame] = {
+      Try(spark.read
+        .format("dpla.ingestion3.harvesters.oai")
+        .options(readerOptions)
+        .load())
+    }
 
     runHarvest() match {
       case Success(results) =>
@@ -75,12 +84,7 @@ object OaiHarvesterMain {
 
       case Failure(f) => logger.fatal(s"Unable to harvest records. ${f.getMessage}")
     }
-
     // Stop spark session.
     sc.stop()
-
-    val end = System.currentTimeMillis()
-
-    Utils.printResults((end-start),recordsHarvestedCount)
   }
 }
