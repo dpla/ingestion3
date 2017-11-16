42e536730cd3a5ca36453bc0c1ce31712c3610ef
Audrey Altman <audrey@dp.la>
2017-11-16 13:41:49 -0500

    change case classes

--- src/main/scala/dpla/ingestion3/harvesters/oai/OaiResponse.scala
+++ src/main/scala/dpla/ingestion3/harvesters/oai/OaiResponse.scala
@@ -5,6 +5,7 @@ sealed trait OaiResponse
 
 // Url and text are optional b/c there may be errors in formulating the URL or
 // parsing the XML response.
+@Deprecated
 case class OaiSource(queryParams: Map[String, String],
                      url: Option[String] = None,
                      text: Option[String] = None) extends OaiResponse
@@ -19,17 +20,16 @@ case class SetsPage(sets: Seq[OaiSet]) extends OaiResponse
 
 // An error that occurs during the harvest, but that does not cause total failure.
 case class OaiError(message: String,
-                    errorSource: OaiSource) extends OaiResponse
+                    url: Option[String] = None) extends OaiResponse
 
 // A single record from an OAI Harvest.
 case class OaiRecord(id: String,
                      document: String,
-                     setIds: Seq[String],
-                     recordSource: OaiSource)
+                     setIds: Seq[String])
 
 // A single set from an OAI harvest.
 case class OaiSet(id: String,
-                  document: String,
-                  setSource: OaiSource)
+                  document: String)
 
+// A single page response from an Oai feed.  The page may include an error message.
 case class OaiPage(page: String) extends OaiResponse
