d804569be1f68835aab8676fa7f5e6a7fb14ccab
Michael Della Bitta <mdellabitta@gmail.com>
2017-11-16 12:20:59 -0500

    Added top level comments.

--- src/main/scala/dpla/ingestion3/harvesters/oai/refactor/WhitelistOaiRelation.scala
+++ src/main/scala/dpla/ingestion3/harvesters/oai/refactor/WhitelistOaiRelation.scala
@@ -3,6 +3,13 @@ package dpla.ingestion3.harvesters.oai.refactor
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SQLContext}
 
+/**
+  * OaiRelation for harvests that want to havest all sets *except* those specified.
+  *
+  * @param whitelistHarvest Configuration information.
+  * @param oaiMethods Implementation of the OaiMethods trait.
+  * @param sqlContext Spark sqlContext.
+  */
 class WhitelistOaiRelation(whitelistHarvest: WhitelistHarvest)
                           (@transient oaiMethods: OaiMethods)
                           (@transient override val sqlContext: SQLContext)
