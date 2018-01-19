559aca80fe937283847a20f765501a25610f26d2
Scott Williams <scott@dp.la>
2018-01-18 20:58:51 -0500

    Uncomment Harvest Exector/Entry

--- src/main/scala/dpla/ingestion3/entries/ingest/HarvestEntry.scala
+++ src/main/scala/dpla/ingestion3/entries/ingest/HarvestEntry.scala
@@ -1,36 +1,36 @@
-//package dpla.ingestion3.entries.ingest
-//
-//import dpla.ingestion3.confs.{CmdArgs, Ingestion3Conf, i3Conf}
-//import dpla.ingestion3.executors.HarvestExecutor
-//import dpla.ingestion3.utils.Utils
-//
-///**
-//  * Entry point for running a harvest.
-//  *
-//  * Expects three command-line args:
-//  *   --output  Path to output directory
-//  *   --conf    Path to conf file
-//  *   --name    Provider short name
-//  */
-//object HarvestEntry extends HarvestExecutor {
-//
-//  def main(args: Array[String]): Unit = {
-//
-//    // Read in command line args.
-//    val cmdArgs = new CmdArgs(args)
-//
-//    val dataOut = cmdArgs.getOutput()
-//    val confFile = cmdArgs.getConfigFile()
-//    val shortName = cmdArgs.getProviderName()
-//
-//    // Get mapping logger.
-//    val harvestLogger = Utils.createLogger("harvest", shortName)
-//
-//    // Load configuration from file.
-//    val i3Conf = new Ingestion3Conf(confFile, Some(shortName))
-//    val providerConf: i3Conf = i3Conf.load()
-//
-//    // Execute harvest.
-//    execute(shortName, dataOut, providerConf, harvestLogger)
-//  }
-//}
+package dpla.ingestion3.entries.ingest
+
+import dpla.ingestion3.confs.{CmdArgs, Ingestion3Conf, i3Conf}
+import dpla.ingestion3.executors.HarvestExecutor
+import dpla.ingestion3.utils.Utils
+
+/**
+  * Entry point for running a harvest.
+  *
+  * Expects three command-line args:
+  *   --output  Path to output directory
+  *   --conf    Path to conf file
+  *   --name    Provider short name
+  */
+object HarvestEntry extends HarvestExecutor {
+
+  def main(args: Array[String]): Unit = {
+
+    // Read in command line args.
+    val cmdArgs = new CmdArgs(args)
+
+    val dataOut = cmdArgs.getOutput()
+    val confFile = cmdArgs.getConfigFile()
+    val shortName = cmdArgs.getProviderName()
+
+    // Get mapping logger.
+    val harvestLogger = Utils.createLogger("harvest", shortName)
+
+    // Load configuration from file.
+    val i3Conf = new Ingestion3Conf(confFile, Some(shortName))
+    val providerConf: i3Conf = i3Conf.load()
+
+    // Execute harvest.
+    execute(shortName, dataOut, providerConf, harvestLogger)
+  }
+}
