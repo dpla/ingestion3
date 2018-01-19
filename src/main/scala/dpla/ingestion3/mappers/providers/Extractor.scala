2772b0622ca89056743a6c5fb223664655ad2649
Scott Williams <scott@dp.la>
2018-01-18 21:00:27 -0500

    Delete old Extractor trait

--- src/main/scala/dpla/ingestion3/mappers/providers/Extractor.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/Extractor.scala
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 package dpla.ingestion3.mappers.providers
 
-import java.io
 import java.net.URI
 
 import dpla.ingestion3.model.{EdmAgent, OreAggregation}
@@ -113,25 +111,3 @@ trait Extractor {
 }
 
 case class ExtractorException(message: String) extends Exception(message)
-=======
-//package dpla.ingestion3.mappers.providers
-//
-//import java.net.URI
-//
-//import dpla.ingestion3.model.{EdmAgent, OreAggregation}
-//import org.apache.commons.codec.digest.DigestUtils
-//
-//import scala.util.{Failure, Success, Try}
-//
-///**
-//  * Interface that all provider extractors implement.
-//  */
-//
-//trait Extractor {
-//
-//  def build(): Try[OreAggregation]
-//
-//}
-//
-//case class ExtractorException(message: String) extends Exception(message)
->>>>>>> DO NOT MERGE
