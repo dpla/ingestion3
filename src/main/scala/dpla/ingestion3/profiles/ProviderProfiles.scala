4911e4a498d9f4ce48288266427c28a27581eb98
Scott Williams <scott@dp.la>
2018-05-09 09:22:10 -0400

    Replace placeholder value for P2P mapping

--- src/main/scala/dpla/ingestion3/profiles/ProviderProfiles.scala
+++ src/main/scala/dpla/ingestion3/profiles/ProviderProfiles.scala
@@ -72,10 +72,10 @@ class OhioProfile extends XmlProfile {
   * - Colorado and Wyoming
   */
 class P2PProfile extends JsonProfile {
-  type Mapping = CdlMapping // FIXME Placehoder
+  type Mapping = _
 
   override def getHarvester = classOf[P2PFileHarvester]
-  override def getMapping = new CdlMapping
+  override def getMapping = _
 }
 /**
   * Pennsylvania Hub
