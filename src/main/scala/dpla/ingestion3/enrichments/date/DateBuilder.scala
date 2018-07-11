514e931887d0a905e2339ea13629e64729eb71aa
Scott Williams <scott@dp.la>
2018-07-11 14:02:17 -0400

    WIP Initial rewrite of date parsing and enrichment tests

--- src/main/scala/dpla/ingestion3/enrichments/date/DateBuilder.scala
+++ src/main/scala/dpla/ingestion3/enrichments/date/DateBuilder.scala
@@ -1,18 +1,27 @@
 package dpla.ingestion3.enrichments.date
 
-import java.util.Locale
-
+import org.joda.time.LocalDate
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter, DateTimeFormatterBuilder}
-import org.joda.time.{DateTime, LocalDate, format}
 
 import scala.util.{Failure, Success, Try}
 
+/**
+  *
+  * @param regexPattern Patten to match against
+  * @param dateTimePattern Pattern to transform into
+  * @param label Label for this pattern to pattern transformation
+  */
 case class EdtfPatternMap(regexPattern: String, dateTimePattern: String, label: String)
 
 
+/**
+  *
+  */
 object DateBuilderPatterns {
+  // Supported delimiters in date values
   protected val delim = "\\s*[\\/-]*\\s*"
 
+  // Support date patterns
   val dateTimeFormatPatterns = Array(
     DateTimeFormat.forPattern("yyyy").getParser,
     DateTimeFormat.forPattern("yyyy MM dd").getParser,
@@ -24,10 +33,14 @@ object DateBuilderPatterns {
   val formatter: DateTimeFormatter = new DateTimeFormatterBuilder().append(null, dateTimeFormatPatterns).toFormatter
 }
 
+/**
+  *
+  */
 class DateBuilder {
   /**
+    * Parses a string value into a Joda LocalDate object
     *
-    * @param date
+    * @param date Original date value to parse
     * @return
     */
   def buildDateObject(date: String): Option[LocalDate] = {
@@ -41,9 +54,11 @@ class DateBuilder {
   }
 
   /**
+    * Normalizes a provided date string to facilitate pattern matching
+    * - Replace all supported delimiters with whitespace
     *
-    * @param date
-    * @return
+    * @param date Date string
+    * @return Normalized date string
     */
   def normalizeDate(date: String): String = date.replaceAll("[\\/-]", " ")
 }
--- src/test/scala/dpla/ingestion3/enrichments/DateBuilderTest.scala
+++ src/test/scala/dpla/ingestion3/enrichments/DateBuilderTest.scala
@@ -10,34 +10,51 @@ import org.scalatest.prop.Checkers
 class DateBuilderTest extends FlatSpec with BeforeAndAfter with Matchers with Checkers {
 
   case class DateTestObj(
-                          day: String,
-                          month: String,
-                          year: String,
+                          day: String = "",
+                          month: String = "",
+                          year: String = "",
                           delimiter: String = " "
                         )
 
   val dateBulder= new DateBuilder()
   val yearGen: Gen[Int] = Gen.choose(1000,2018)
   val dayGen: Gen[String] = Gen.oneOf(
-    "01", //"1",
-    "02", //"2",
-    "03", //"3",
-    "04", //"4",
-    "05", //"5",
-    "06", //"6",
-    "07", //"7",
-    "08", //"8",
-    "09", // "9",
+    "01", "1",
+    "02", "2",
+    "03", "3",
+    "04", "4",
+    "05", "5",
+    "06", "6",
+    "07", "7",
+    "08", "8",
+    "09", "9",
     "10", "11", "12",
     "13", "14", "15",
     "16", "17", "18",
     "19", "20", "21",
     "22", "23", "24",
     "25", "26", "27",
-    "28", "29", "30") // FIXME how to deal with Feb 29, Nov 31
+    "28")
+    // "29", "30") // FIXME how to deal with Feb 29, Nov 31
   val monthStrGen: Gen[String] = Gen.oneOf(
     "Jan", "Feb", "Mar", "Apr", "May", "Jun",
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+  val monthStrIntMap: Map[String, String] = Map(
+    "Jan" -> "01",
+    "Feb" -> "02",
+    "Mar" -> "03",
+    "Apr" -> "04",
+    "May" -> "05",
+    "Jun" -> "06",
+    "Jul" -> "07",
+    "Aug" -> "08",
+    "Sep" -> "09",
+    "Oct" -> "10",
+    "Nov" -> "11",
+    "Dec" -> "12"
+  )
+
   val monthGen: Gen[String] = Gen.oneOf(
     "01", "1",
     "02", "2",
@@ -84,13 +101,22 @@ class DateBuilderTest extends FlatSpec with BeforeAndAfter with Matchers with Ch
   }
 
   it should "create a valid label for yyyy MMM" in {
-    val date = "1945 Mar"
-      val expectedDate = Option("1945-03-01")
-      val generatedDate = dateBulder.buildDateObject(date) match {
+    // val date = "1945 Mar"
+    val myGen = for {
+      month <- monthStrGen
+      year <- yearGen
+    } yield DateTestObj(month = month, year = year.toString)
+
+    check(Prop.forAllNoShrink(myGen) { date =>
+      val dateStr = s"${date.year}${date.delimiter}${date.month}"
+      val expectedDate = Option(s"${date.year}-${monthStrIntMap.getOrElse(date.month, "00")}-01")
+      val generatedDate = dateBulder.buildDateObject(dateStr) match {
         case Some(d) => Some(d.toString)
         case None => None
       }
+
       generatedDate === expectedDate
+    })
   }
 
   it should "create a valid label for MMM yyyy" in {
