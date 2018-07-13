74ca09fec13854aa4f4e798d9495d198560dea56
Scott Williams <scott@dp.la>
2018-07-13 09:37:08 -0400

    Cleanup and reorg

--- src/main/scala/dpla/ingestion3/enrichments/date/DateBuilder.scala
+++ src/main/scala/dpla/ingestion3/enrichments/date/DateBuilder.scala
@@ -1,6 +1,6 @@
 package dpla.ingestion3.enrichments.date
 
-import org.joda.time.{LocalDate, format}
+import org.joda.time.LocalDate
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter, DateTimeFormatterBuilder}
 
 import scala.util.{Failure, Success, Try}
@@ -17,9 +17,6 @@ case class EdtfPatternMap(regexPattern: String, dateTimePattern: String, label: 
   *
   */
 object DateBuilderPatterns {
-  // Supported delimiters in date values
-  protected val delim = "\\s*[\\/-]*\\s*"
-
   // Support date patterns
   val dateTimeFormatPatterns = Array(
     // Single date
@@ -30,21 +27,20 @@ object DateBuilderPatterns {
     DateTimeFormat.forPattern("yyyy MMM dd").getParser,
     DateTimeFormat.forPattern("MMM yyyy").getParser
 
-    // Patterns to be implemented
-    // yyyy-MM | 1934-01
 
+    // TODO Patterns to be implemented
     // Date ranges
-    // yyyy-yyyy | 1935-1956
-    // yyyy-MM-dd/yyyy-MM-dd | 1850-01-01/1950-12-31
+    //  yyyy-yyyy | 1935-1956
+    //  yyyy-MM-dd/yyyy-MM-dd | 1850-01-01/1950-12-31
 
     // Periods
-    // century -> | 19th century
+    //  century -> | 19th century
 
     // Indeterminate
-    //    "n.d." -> "indeterminate (n.d.)", // n.d.
-    //    "un" -> "indeterminate (un*)", // unknown
-    //    "no" -> "indeterminate (no*)", // no(t) dated
-    //    "^[a-zA-Z]*$" -> "indeterminate (no digits)"  // early bronze age
+    //  "n.d." -> "indeterminate (n.d.)", // n.d.
+    //  "un" -> "indeterminate (un*)", // unknown
+    //  "no" -> "indeterminate (no*)", // no(t) dated
+    //  "^[a-zA-Z]*$" -> "indeterminate (no digits)"  // early bronze age
   )
 
   val formatter: DateTimeFormatter = new DateTimeFormatterBuilder().append(null, dateTimeFormatPatterns).toFormatter
@@ -54,6 +50,7 @@ object DateBuilderPatterns {
   *
   */
 class DateBuilder {
+  protected val delimiters = "\\s*[\\/-]*\\s*"
   /**
     * Parses a string value into a Joda LocalDate object
     *
@@ -72,10 +69,10 @@ class DateBuilder {
 
   /**
     * Normalizes a provided date string to facilitate pattern matching
-    * - Replace all supported delimiters with whitespace
+    * - Replace all supported delimiters (forward slash, dash) with whitespace
     *
     * @param date Date string
     * @return Normalized date string
     */
-  def normalizeDate(date: String): String = date.replaceAll("[\\/-]", " ")
+  def normalizeDate(date: String): String = date.replaceAll(delimiters, " ")
 }
