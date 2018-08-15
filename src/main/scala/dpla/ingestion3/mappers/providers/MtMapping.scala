97e3c70576271beff4bd2fa5f0a77f232bc21e10
Scott Williams <scott@dp.la>
2018-08-15 13:41:57 -0400

    Initial MT mapping

---
+++ src/main/scala/dpla/ingestion3/mappers/providers/MtMapping.scala
@@ -0,0 +1,148 @@
+package dpla.ingestion3.mappers.providers
+
+import java.net.URI
+
+import dpla.ingestion3.enrichments.normalizations.StringNormalizationUtils._
+import dpla.ingestion3.enrichments.normalizations.filters.{DigitalSurrogateBlockList, FormatTypeValuesBlockList}
+import dpla.ingestion3.mappers.utils.{Document, IdMinter, Mapping, XmlExtractor}
+import dpla.ingestion3.messages.{IngestMessage, MessageCollector}
+import dpla.ingestion3.model.DplaMapData.{ExactlyOne, ZeroToOne}
+import dpla.ingestion3.model._
+import dpla.ingestion3.utils.Utils
+import org.json4s.JValue
+import org.json4s.JsonDSL._
+
+import scala.xml._
+
+
+class MtMapping extends Mapping[NodeSeq] with XmlExtractor with IdMinter[NodeSeq] {
+
+  val formatBlockList: Set[String] =
+    DigitalSurrogateBlockList.termList ++
+      FormatTypeValuesBlockList.termList
+
+  // ID minting functions
+  override def useProviderName(): Boolean = true
+
+  override def getProviderName(): String = "mt"
+
+  override def getProviderId(implicit data: Document[NodeSeq]): String =
+    extractString(data \ "header" \ "identifier")
+      .getOrElse(throw new RuntimeException(s"No ID for record $data")
+      )
+
+  // SourceResource mapping
+  override def collection(data: Document[NodeSeq]): Seq[DcmiTypeCollection] =
+    // first instance of <mods:relatedItem><mods:titleInfo><mods:title>
+    extractStrings(data \\ "relatedItem" \ "titleInfo" \ "title")
+      .map(nameOnlyCollection)
+      .take(1)
+
+  override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
+    // <mods:name><mods:namePart> when <mods:role><mods:roleTerm> equals Creator
+  (data \\ "name")
+    .map(node => node.filter(n => (n \\ "roleTerm").text.equalsIgnoreCase("creator")))
+    .flatMap(n => extractStrings(n \ "namePart"))
+    .map(nameOnlyAgent)
+
+  override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
+  // <mods:originInfo><mods:dateCreated>
+    extractStrings(data \\ "originInfo" \ "dateCreated")
+      .map(stringOnlyTimeSpan)
+
+  override def description(data: Document[NodeSeq]): Seq[String] =
+  // <mods:note> @type=content
+    extractStrings(data \ "metadata" \\ "description") ++
+      extractStrings(data \ "metadata" \\ "source")
+
+  override def format(data: Document[NodeSeq]): Seq[String] =
+    (extractStrings(data \ "metadata" \\ "format") ++
+      extractStrings(data \ "metadata" \\ "type"))
+      .map(_.applyBlockFilter(formatBlockList))
+      .filter(_.nonEmpty)
+
+  override def identifier(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "identifier")
+
+  override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \ "metadata" \\ "language")
+      .map(nameOnlyConcept)
+
+  override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
+    extractStrings(data \ "metadata" \\ "Place")
+      .map(nameOnlyPlace)
+
+  override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "metadata" \\ "publisher")
+      .map(nameOnlyAgent)
+
+  override def rights(data: Document[NodeSeq]): Seq[String] =
+    (data \ "metadata" \\ "rights").flatMap(r => {
+      r.prefix match {
+        case "dc" => Option(r.text)
+        case _ => None
+      }
+    })
+
+  override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \ "metadata" \\ "subject")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyConcept)
+
+  override def title(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "title")
+
+  override def `type`(data: Document[NodeSeq]): Seq[String] =
+    (extractStrings(data \ "metadata" \\ "type") ++
+      extractStrings(data \ "metadata" \\ "format"))
+      .flatMap(_.splitAtDelimiter(";"))
+
+
+  // OreAggregation
+  override def dplaUri(data: Document[NodeSeq]): URI = mintDplaItemUri(data)
+
+  override def dataProvider(data: Document[NodeSeq])
+                           (implicit msgCollector: MessageCollector[IngestMessage]): EdmAgent = {
+    extractStrings(data \ "metadata" \\ "dataProvider")
+      .map(nameOnlyAgent)
+      .headOption // take the first value
+      .getOrElse( // return the first value or throw an exception
+      throw new Exception(s"Missing required property metadata/dataProvider is empty for ${getProviderId(data)}")
+    )
+  }
+
+  override def edmRights(data: Document[NodeSeq]): ZeroToOne[URI] = {
+    (data \ "metadata" \\ "rights").flatMap(r => r.prefix match {
+      case "edm" => Option(Utils.createUri(r.text))
+      case _ => None
+    }).headOption
+  }
+
+  override def isShownAt(data: Document[NodeSeq])
+                        (implicit msgCollector: MessageCollector[IngestMessage]): EdmWebResource =
+    uriOnlyWebResource(
+      Utils.createUri(extractStrings(data \ "metadata" \\ "isShownAt")
+        .headOption
+        .getOrElse(
+          throw new RuntimeException(s"No isShownAt property in record ${getProviderId(data)}")
+        )))
+
+  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = Utils.formatXml(data)
+
+  override def preview(data: Document[NodeSeq])
+                      (implicit msgCollector: MessageCollector[IngestMessage]): ZeroToOne[EdmWebResource] =
+    extractStrings(data \ "metadata" \\ "preview")
+      .map(uri => uriOnlyWebResource(Utils.createUri(uri)))
+      .headOption
+
+  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = agent
+
+  override def sidecar(data: Document[NodeSeq]): JValue =
+    ("prehashId" -> buildProviderBaseId()(data)) ~ ("dplaId" -> mintDplaId(data))
+
+  // Helper method
+  def agent = EdmAgent(
+    name = Some("Big Sky Country Digital Network"),
+    uri = Some(Utils.createUri("http://dp.la/api/contributor/dc"))
+  )
+}
