65b5bdb3ba9c847956943e2953f76ed43f6b4c38
Audrey Altman <audrey@dp.la>
2018-10-11 16:04:16 -0400

    get most recent harvest IngestRemap

--- src/main/scala/dpla/ingestion3/entries/ingest/IngestRemap.scala
+++ src/main/scala/dpla/ingestion3/entries/ingest/IngestRemap.scala
@@ -1,7 +1,7 @@
 package dpla.ingestion3.entries.ingest
 
 import dpla.ingestion3.confs.{CmdArgs, Ingestion3Conf}
-import dpla.ingestion3.dataStorage._
+import dpla.ingestion3.dataStorage.InputHelper
 import dpla.ingestion3.executors.{EnrichExecutor, JsonlExecutor, MappingExecutor}
 import dpla.ingestion3.entries.reports.ReporterMain._
 import dpla.ingestion3.utils.Utils
@@ -46,32 +46,32 @@ object IngestRemap extends MappingExecutor
 
     logger.info(s"Using harvest data from $harvestData")
 
-//    // Load configuration from file.
-//    val i3Conf = new Ingestion3Conf(confFile, Some(shortName))
-//    val conf = i3Conf.load()
-//
-//    // Read spark master property from conf, default to 'local[1]' if not set
-//    val sparkMaster = conf.spark.sparkMaster.getOrElse("local[1]")
-//
-//    val sparkConf = new SparkConf()
-//      .setAppName(s"Mapping: $shortName")
-//      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-//      .set("spark.kryoserializer.buffer.max", "200")
-//      .setMaster(sparkMaster)
-//
-//    // TODO These processes should return some flag or metric to help determine whether to proceed
-//    // Mapping
-//    val mapDataOut: String =
-//      executeMapping(sparkConf, harvestData, baseDataOut, shortName, logger)
-//
-//    // Enrichment
-//    val enrichDataOut: String =
-//      executeEnrichment(sparkConf, mapDataOut, baseDataOut, shortName, logger, conf)
-//
-//    // Json-l
-//    executeJsonl(sparkConf, enrichDataOut, baseDataOut, shortName, logger)
-//
-//    // Reports
-//    executeAllReports(sparkConf, enrichDataOut, baseDataOut, shortName, logger)
+    // Load configuration from file.
+    val i3Conf = new Ingestion3Conf(confFile, Some(shortName))
+    val conf = i3Conf.load()
+
+    // Read spark master property from conf, default to 'local[1]' if not set
+    val sparkMaster = conf.spark.sparkMaster.getOrElse("local[1]")
+
+    val sparkConf = new SparkConf()
+      .setAppName(s"Mapping: $shortName")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .set("spark.kryoserializer.buffer.max", "200")
+      .setMaster(sparkMaster)
+
+    // TODO These processes should return some flag or metric to help determine whether to proceed
+    // Mapping
+    val mapDataOut: String =
+      executeMapping(sparkConf, harvestData, baseDataOut, shortName, logger)
+
+    // Enrichment
+    val enrichDataOut: String =
+      executeEnrichment(sparkConf, mapDataOut, baseDataOut, shortName, logger, conf)
+
+    // Json-l
+    executeJsonl(sparkConf, enrichDataOut, baseDataOut, shortName, logger)
+
+    // Reports
+    executeAllReports(sparkConf, enrichDataOut, baseDataOut, shortName, logger)
   }
 }
