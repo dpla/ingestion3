b1119cd795ea6106b04c94ad26ea3ab29e030a11
scott <scott.brandon.williams@gmail.com>
2018-11-05 10:43:48 -0500

    Initial draft of Getty mapping (#236)
    Update to Getty entry in ProviderProfile
    Getty mapping tests and associated test data

---
+++ src/main/scala/dpla/ingestion3/mappers/providers/GettyMapping.scala
@@ -0,0 +1,148 @@
+package dpla.ingestion3.mappers.providers
+
+import dpla.ingestion3.enrichments.normalizations.StringNormalizationUtils._
+import dpla.ingestion3.enrichments.normalizations.filters.ExtentIdentificationList
+import dpla.ingestion3.mappers.utils.{Document, IdMinter, Mapping, XmlExtractor}
+import dpla.ingestion3.model.DplaMapData._
+import dpla.ingestion3.model.{nameOnlyAgent, _}
+import dpla.ingestion3.utils.Utils
+import org.json4s.JValue
+import org.json4s.JsonDSL._
+
+import scala.xml._
+
+
+class GettyMapping extends Mapping[NodeSeq] with XmlExtractor with IdMinter[NodeSeq] {
+
+  val extentAllowList: Set[String] =
+    ExtentIdentificationList.termList
+
+  // ID minting functions
+  override def useProviderName(): Boolean = true
+
+  override def getProviderName(): String = "getty"
+
+  override def getProviderId(implicit data: Document[NodeSeq]): String =
+    extractString(data \\ "PrimoNMBib" \ "record" \ "control" \ "recordid")
+      .getOrElse(throw new RuntimeException(s"No ID for record $data"))
+
+  // SourceResource mapping
+  override def collection(data: Document[NodeSeq]): Seq[DcmiTypeCollection] =
+    // display/lds43 AND display/lds34
+    (extractStrings(data \\ "display" \ "lds43") ++
+      extractStrings(data \\ "display" \ "lds34"))
+      .map(nameOnlyCollection)
+
+  override def contributor(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \\ "display" \ "contributor")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyAgent)
+
+  override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
+    (extractStrings(data \\ "display" \ "creator") ++
+      extractStrings(data \\ "display" \ "lds50"))
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyAgent)
+
+  override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
+    // display/creationdate
+      extractStrings(data \\ "display" \ "creationdate")
+      .map(stringOnlyTimeSpan)
+
+  override def description(data: Document[NodeSeq]): Seq[String] =
+  // display/lds04 AND display/lds28
+    extractStrings(data \\ "display" \ "lds04") ++
+      extractStrings(data \\ "display" \ "lds28")
+
+  override def extent(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \\ "display" \ "format")
+      .map(_.applyAllowFilter(extentAllowList))
+
+  override def format(data: Document[NodeSeq]): ZeroToMany[String] =
+    (extractStrings(data \\ "display" \ "lds09") ++
+      extractStrings(data \\ "display" \ "format"))
+      .map(_.applyBlockFilter(extentAllowList))
+
+  override def identifier(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \\ "display" \ "lds14")
+      .flatMap(_.splitAtDelimiter(";"))
+
+  override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \\ "display" \ "language")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyConcept)
+
+  override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
+    extractStrings(data \\ "display" \ "coverage")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyPlace)
+
+  override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \\ "display" \ "publisher")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyAgent)
+
+  override def rights(data: Document[NodeSeq]): AtLeastOne[String] =
+    // display/lds27 AND display/rights
+    ((data \\ "display" \ "rights") ++
+      (data \\ "display" \ "lds27"))
+      .flatMap(extractStrings)
+
+  override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
+  // display/subject AND display/lds49
+    (extractStrings(data \\ "display" \ "subject") ++
+      extractStrings(data \\ "display" \ "lds49"))
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyConcept)
+
+  override def title(data: Document[NodeSeq]): Seq[String] =
+    // display/title AND display/lds03
+    (extractStrings(data \\ "display" \ "title") ++
+      extractStrings(data \\ "display" \ "lds03"))
+      .flatMap(_.splitAtDelimiter(";"))
+
+  override def `type`(data: Document[NodeSeq]): Seq[String] =
+    // display/lds26
+    extractStrings(data \\ "display" \ "lds26")
+
+  // OreAggregation
+  override def dplaUri(data: Document[NodeSeq]): URI = mintDplaItemUri(data)
+
+  override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    Seq(nameOnlyAgent("Getty Research Institute"))
+
+  override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] = {
+    val baseIsShownAt =
+      "http://primo.getty.edu/primo_library/libweb/action/dlDisplay.do?vid=GRI-OCP&afterPDS=true&institution=01GRI&docId="
+
+    extractString(data \\"control" \ "sourceid") match {
+      case Some("GETTY_ROSETTA") =>
+        extractStrings(data \\ "display" \ "lds29")
+          .map(stringOnlyWebResource)
+      case Some("GETTY_OCP") =>
+        extractStrings(data \\ "control" \ "recordid")
+          .map(baseIsShownAt + _.trim)
+          .map(stringOnlyWebResource)
+      case _ => Seq()
+    }
+  }
+
+  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = Utils.formatXml(data)
+
+  override def preview(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
+    // sear/thumbnail
+    extractStrings(data \\ "LINKS" \ "thumbnail")
+      .map(stringOnlyWebResource)
+
+  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = agent
+
+  override def sidecar(data: Document[NodeSeq]): JValue =
+    ("prehashId" -> buildProviderBaseId()(data)) ~ ("dplaId" -> mintDplaId(data))
+
+  // Helper method
+  def agent = EdmAgent(
+    name = Some("J. Paul Getty Trust"),
+    uri = Some(URI("http://dp.la/api/contributor/getty"))
+  )
+}
+
---
+++ src/test/scala/dpla/ingestion3/mappers/providers/GettyMappingTest.scala
@@ -0,0 +1,81 @@
+package dpla.ingestion3.mappers.providers
+
+import dpla.ingestion3.mappers.utils.Document
+import dpla.ingestion3.messages.{IngestMessage, MessageCollector}
+import dpla.ingestion3.model._
+import dpla.ingestion3.utils.FlatFileIO
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+
+import scala.xml.{NodeSeq, XML}
+
+class GettyMappingTest extends FlatSpec with BeforeAndAfter {
+
+  implicit val msgCollector: MessageCollector[IngestMessage] = new MessageCollector[IngestMessage]
+  val shortName = "getty"
+  val xmlString: String = new FlatFileIO().readFileAsString("/getty.xml")
+  val xml: Document[NodeSeq] = Document(XML.loadString(xmlString))
+  val extractor = new GettyMapping
+
+  it should "use the provider shortname in minting IDs " in
+    assert(extractor.useProviderName())
+
+  it should "extract the correct provider identifier " in
+    assert(extractor.getProviderId(xml) === "GETTY_OCPFL608236")
+
+  it should "extract the correct collection titles" in {
+    val expected = Seq("Foto Arte Minore / Max Hutzel (accession number 86.P.8)")
+      .map(nameOnlyCollection)
+    assert(extractor.collection(xml) === expected)
+  }
+
+  it should "extract the correct contributors" in {
+    val expected = Seq("Contributor").map(nameOnlyAgent)
+    assert(extractor.contributor(xml) == expected)
+  }
+  it should "extract the correct dates" in {
+    val expected = Seq("1960-1990").map(stringOnlyTimeSpan)
+    assert(extractor.date(xml) === expected)
+  }
+  it should "extract the correct description" in
+    assert(extractor.description(xml) == Seq("German-born photographer and scholar Max Hutzel (1911-1988) photographed in Italy from the early 1960s until his death. The result of this project, referred to by Hutzel as Foto Arte Minore, is thorough documentation of art historical development in Italy up to the 18th century, including objects of the Etruscans and the Romans, as well as early Medieval, Romanesque, Gothic, Renaissance and Baroque monuments. Images are organized by geographic region in Italy, then by province, city, site complex and monument.", "Medieval: Entire manuscript. Manuscripts. Post-medieval: Sculpture and ceramics from local workshops (2nd half 12th century; Sienese maiolica; 14th century weapons"))
+
+  it should "extract the correct place values" in {
+    val expected = Seq("Place", "Place2").map(nameOnlyPlace)
+    assert(extractor.place(xml) === expected)
+  }
+  it should "extract the correct publishers" in {
+    val expected = Seq("Publisher").map(nameOnlyAgent)
+    assert(extractor.publisher(xml) === expected)
+  }
+  it should "extract the correct rights value" in {
+    assert(extractor.rights(xml) === Seq("Digital images courtesy of the Getty's Open Content Program."))
+  }
+  it should "extract the correct subjects" in {
+    val expected = Seq("Subject").map(nameOnlyConcept)
+    assert(extractor.subject(xml) === expected)
+  }
+  it should "extract the correct titles" in {
+    val expected = Seq("Tuscany--Siena--Montalcino--Museo Civico, Image 150")
+    assert(extractor.title(xml) === expected)
+  }
+  it should "extract the correct types" in {
+    val expected = Seq("Still image")
+    assert(extractor.`type`(xml) === expected)
+  }
+  it should "extract the correct dataProvider" in {
+    val expected = Seq(nameOnlyAgent("Getty Research Institute"))
+    assert(extractor.dataProvider(xml) === expected)
+  }
+  it should "extract the correct isShownAt" in {
+    val expected = Seq(uriOnlyWebResource(URI("http://primo.getty.edu/primo_library/libweb/action/dlDisplay.do?vid=GRI-OCP&afterPDS=true&institution=01GRI&docId=GETTY_OCPFL608236")))
+    assert(extractor.isShownAt(xml) === expected)
+  }
+  it should "extract the correct preview" in {
+    val expected = Seq(
+      "http://rosettaapp.getty.edu:1801/delivery/DeliveryManagerServlet?dps_pid=FL608236",
+      "http://rosettaapp.getty.edu:1801/delivery/DeliveryManagerServlet?dps_pid=FL608236")
+      .map(stringOnlyWebResource)
+    assert(extractor.preview(xml) === expected)
+  }
+}
+
