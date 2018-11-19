56e5ee1284853e30d2860c55d624ac76f01ea488
Audrey Altman <audrey@dp.la>
2018-11-19 17:01:13 -0500

    initial testing suite working

---
+++ src/main/scala/dpla/ingestion3/mappers/providers/VirginiaMapping.scala
@@ -0,0 +1,161 @@
+package dpla.ingestion3.mappers.providers
+
+import dpla.ingestion3.enrichments.normalizations.StringNormalizationUtils._
+import dpla.ingestion3.enrichments.normalizations.filters.{DigitalSurrogateBlockList, ExtentIdentificationList, FormatTypeValuesBlockList}
+import dpla.ingestion3.mappers.utils.{Document, IdMinter, Mapping, XmlExtractor}
+import dpla.ingestion3.messages.{IngestMessage, IngestMessageTemplates, MessageCollector}
+
+import dpla.ingestion3.model.DplaMapData.{ExactlyOne, LiteralOrUri, ZeroToMany, ZeroToOne}
+import dpla.ingestion3.model.{uriOnlyWebResource, _}
+import dpla.ingestion3.utils.Utils
+import org.json4s.JValue
+import org.json4s.JsonDSL._
+
+import scala.util.{Failure, Success}
+import scala.xml._
+
+
+class VirginiaMapping extends Mapping[NodeSeq] with XmlExtractor with IdMinter[NodeSeq]
+  with IngestMessageTemplates {
+
+  // ID minting functions
+  override def useProviderName(): Boolean = false
+
+  override def getProviderName(): String = "virginia"
+
+  // TODO: What is persistent ID?
+  override def getProviderId(implicit data: Document[NodeSeq]): String =
+    extractString(data \ "header" \ "identifier")
+      .getOrElse(throw new RuntimeException(s"No ID for record $data"))
+
+  // SourceResource mapping
+  override def alternateTitle(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "metadata" \\ "alternative")
+
+  // Only use the first isPartOf instance
+  override def collection(data: Document[NodeSeq]): Seq[DcmiTypeCollection] =
+    extractStrings(data \ "metadata" \\ "isPartOf")
+      .map(nameOnlyCollection)
+
+  override def contributor(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "metadata" \\ "contributor")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyAgent)
+
+  override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "metadata" \\ "creator")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyAgent)
+
+  override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
+    extractStrings(data \ "metadata" \\ "date")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(stringOnlyTimeSpan)
+
+  override def description(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "description")
+
+  override def extent(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "metadata" \\ "extent") ++
+      extentFromFormat(data)
+
+  override def format(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "format")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(_.applyBlockFilter(
+        DigitalSurrogateBlockList.termList ++
+          FormatTypeValuesBlockList.termList ++
+          ExtentIdentificationList.termList))
+      .filter(_.nonEmpty)
+
+  override def identifier(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "identifier")
+
+  override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \ "metadata" \\ "language")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(nameOnlyConcept)
+
+  override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
+    extractStrings(data \ "metadata" \\ "spatial")
+      .flatMap(_.split(";"))
+      .map(nameOnlyPlace)
+
+  override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "metadata" \\ "publisher")
+      .map(nameOnlyAgent)
+
+  override def relation(data: Document[NodeSeq]): Seq[LiteralOrUri] =
+    extractStrings(data \ "metadata" \\ "relation")
+      .map(eitherStringOrUri)
+
+  override def rights(data: Document[NodeSeq]): Seq[String] =
+    (data \ "metadata" \\ "rights").map(r => {
+      r.prefix match {
+        case "dc" => r.text
+        case _ => ""
+      }
+    }).filter(_.nonEmpty)
+
+  override def rightsHolder(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    extractStrings(data \ "metadata" \\ "rightsHolder")
+      .map(nameOnlyAgent)
+
+  override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \ "metadata" \\ "subject")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(_.capitalizeFirstChar)
+      .map(nameOnlyConcept)
+
+  override def title(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "title")
+
+  override def `type`(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "metadata" \\ "type")
+
+  // OreAggregation
+  override def dplaUri(data: Document[NodeSeq]): URI = mintDplaItemUri(data)
+
+  override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    extractStrings(data \ "metadata" \\ "dataProvider")
+      .map(nameOnlyAgent)
+
+  override def edmRights(data: Document[NodeSeq]): ZeroToMany[URI] = {
+    (data \ "metadata" \\ "rights").map(r => r.prefix match {
+      case "edm" => URI(r.text)
+    })
+  }
+
+  override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
+    extractStrings(data \ "metadata" \\ "isShownAt")
+      .map(stringOnlyWebResource)
+
+  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = Utils.formatXml(data)
+
+  override def preview(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
+    extractStrings(data \ "metadata" \\ "preview")
+      .map(stringOnlyWebResource)
+
+  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = agent
+
+  override def sidecar(data: Document[NodeSeq]): JValue =
+    ("prehashId" -> buildProviderBaseId()(data)) ~ ("dplaId" -> mintDplaId(data))
+
+  // Helper method
+  def agent = EdmAgent(
+    name = Some("Ohio Digital Network"),
+    uri = Some(URI("http://dp.la/api/contributor/ohio"))
+  )
+
+  /**
+    * Extracts values from format field and returns values that appear to be
+    * extent statements
+    * @param data
+    * @return
+    */
+  def extentFromFormat(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "metadata" \\ "format")
+      .flatMap(_.splitAtDelimiter(";"))
+      .map(_.extractExtents)
+      .filter(_.nonEmpty)
+}
---
+++ src/test/scala/dpla/ingestion3/mappers/providers/VirginiaMappingTest.scala
@@ -0,0 +1,223 @@
+package dpla.ingestion3.mappers.providers
+
+import dpla.ingestion3.mappers.utils.Document
+import dpla.ingestion3.messages.{IngestMessage, MessageCollector}
+import dpla.ingestion3.utils.FlatFileIO
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import dpla.ingestion3.model._
+
+import scala.xml.{NodeSeq, XML}
+
+class VirginiaMappingTest extends FlatSpec with BeforeAndAfter {
+
+  implicit val msgCollector: MessageCollector[IngestMessage] = new MessageCollector[IngestMessage]
+  val shortName = "virginia"
+  val xmlString: String = new FlatFileIO().readFileAsString("/virginia.xml")
+  val xml: Document[NodeSeq] = Document(XML.loadString(xmlString))
+  val extractor = new VirginiaMapping
+
+  it should "not use the provider shortname in minting IDs "in
+    assert(!extractor.useProviderName())
+
+//  it should "extract the correct provider identifier " in
+//    assert(extractor.getProviderId(xml) === "urn:ohiodplahub.library.ohio.gov:bgsu_12:oai:digitalgallery.bgsu.edu:14058")
+//
+//  it should "throw an Exception if document does not contain a provider identifier" in {
+//    val xml = <record><metadata></metadata></record>
+//    assertThrows[Exception] {
+//      extractor.getProviderId(Document(xml))
+//    }
+//  }
+//
+//  it should "extract the correct alternate titles " in {
+//    val expected = Seq("Alt title one", "Alt title two")
+//    assert(extractor.alternateTitle(xml) === expected)
+//  }
+//
+//  it should "extract the correct collection titles" in {
+//    val expected = Seq("College of Musical Arts Programs", "A second collection")
+//      .map(nameOnlyCollection)
+//    assert(extractor.collection(xml) === expected)
+//  }
+//
+//  it should "extract the correct contributors" in {
+//    val expected = Seq("Kantorksi, Valrie", "Pope, Ann").map(nameOnlyAgent)
+//    assert(extractor.contributor(xml) == expected)
+//  }
+//  it should "extract the correct contributors when the value contains a ';'" in {
+//    val xml: NodeSeq =
+//      <record><metadata>
+//        <dcterms:contributor>Scott; Ted</dcterms:contributor>
+//        <dcterms:contributor>John</dcterms:contributor>
+//      </metadata></record>
+//
+//    val expected = Seq("Scott", "Ted", "John").map(nameOnlyAgent)
+//    assert(extractor.contributor(Document(xml)) == expected)
+//  }
+//
+//  it should "extract the correct creators" in {
+//    val expected = Seq("Sam G.", "Merry B.").map(nameOnlyAgent)
+//    assert(extractor.creator(xml) == expected)
+//  }
+//  it should "extract the correct creators when the value contains a ';'" in {
+//    val xml: NodeSeq =
+//      <record><metadata>
+//        <dcterms:creator>Huey; Lewy</dcterms:creator>
+//        <dcterms:creator>Dewy</dcterms:creator>
+//      </metadata></record>
+//
+//    val expected = Seq("Huey", "Lewy", "Dewy").map(nameOnlyAgent)
+//    assert(extractor.creator(Document(xml)) == expected)
+//  }
+//
+//  it should "extract the correct dates and split around ;" in {
+//    val expected = Seq("1990-02-09", "2008", "2016").map(stringOnlyTimeSpan)
+//    assert(extractor.date(xml) === expected)
+//  }
+//
+//  it should "extract the correct description" in
+//    assert(extractor.description(xml) == Seq("A description", "A second description"))
+//
+//  it should "extract the correct extent" in {
+//    val expected = Seq("21.5 centimeters high, 14 centimeters wide")
+//    assert(extractor.extent(xml) === expected)
+//  }
+//
+//  it should "extract the correct format" in {
+//    val xml: Document[NodeSeq] = Document(<record><metadata>
+//      <dcterms:format>photograph</dcterms:format>
+//    </metadata></record>)
+//    val expected = Seq("photograph")
+//    assert(extractor.format(xml) === expected)
+//  }
+//  it should "extract the correct format and remove values in that are stop words ('image')" in {
+//    val xml: Document[NodeSeq] = Document(<record><metadata>
+//      <dcterms:format>photograph</dcterms:format>
+//      <dcterms:format>1 x 2 x 3</dcterms:format>
+//      <dcterms:format>image</dcterms:format>
+//    </metadata></record>)
+//    val expected = Seq("photograph")
+//    assert(extractor.format(xml) === expected)
+//  }
+//  it should "extract nothing if the format value looks like an extent" in {
+//    val xml: Document[NodeSeq] = Document(<record><metadata>
+//      <dcterms:format>1 score (3 p.) 33 cm</dcterms:format>
+//    </metadata></record>)
+//    val expected = Seq()
+//    assert(extractor.format(xml) === expected)
+//  }
+//  it should "extract the correct format when splitting on ';'  and remove values in that are stop words" in {
+//    val xml: Document[NodeSeq] = Document(<record><metadata>
+//      <dcterms:format>photograph; image</dcterms:format>
+//      <dcterms:format>archival box</dcterms:format>
+//    </metadata></record>)
+//    val expected = Seq("photograph", "archival box")
+//    assert(extractor.format(xml) === expected)
+//  }
+//  it should "extract no format when values are in format blacklist" in {
+//    val xml: Document[NodeSeq] = Document(<record><metadata><dcterms:format>image/jpeg</dcterms:format></metadata></record>)
+//    val expected = Seq()
+//    assert(extractor.format(xml) === expected)
+//  }
+//
+//  it should "extract the correct identifier" in {
+//    val expected = Seq("FA19900209")
+//    assert(extractor.identifier(xml) === expected)
+//  }
+//
+//  it should "extract the correct language when the value contains ';'" in {
+//    val expected = Seq("english", "eng").map(nameOnlyConcept)
+//    assert(extractor.language(xml) === expected)
+//  }
+//  it should "extract the correct language" in {
+//    val xml: NodeSeq =
+//      <record><metadata>
+//        <dcterms:language>english</dcterms:language>
+//        <dcterms:language>eng</dcterms:language>
+//      </metadata></record>
+//    val expected = Seq("english", "eng").map(nameOnlyConcept)
+//    assert(extractor.language(Document(xml)) === expected)
+//  }
+//
+//  it should "extract the correct place values and split around ;" in {
+//    val expected = Seq("Ohio--Terrace Park", "Cincinnati, OH", "Hamilton County", "Butler County").map(nameOnlyPlace)
+//    assert(extractor.place(xml) === expected)
+//  }
+//
+//  it should "extract the correct publishers" in {
+//    val expected = Seq("Hottee Trusty", "Big 'ol publisher").map(nameOnlyAgent)
+//    assert(extractor.publisher(xml) === expected)
+//  }
+//
+//  it should "extract the correct relation" in {
+//    val expected = Seq("College of Musical Arts Programs; MUSIC 003; Music Library " +
+//      "and Bill Schurk Sound Archives; University Libraries; " +
+//      "Bowling Green State University", "Faculty Artist Concert").map(eitherStringOrUri)
+//    assert(extractor.relation(xml) === expected)
+//  }
+//
+//  it should "extract no rights value if the prefix is not 'dc'" in
+//    assert(extractor.rights(xml) === Seq())
+//
+//  it should "extract rights value if the prefix is 'dc'" in {
+//    val rightsXml: Document[NodeSeq] = Document(<record xmlns:dc="http://purl.org/dc/elements/1.1/"><metadata><dc:rights>rights statement</dc:rights></metadata></record>)
+//    val expected = Seq("rights statement")
+//    assert(extractor.rights(rightsXml) === expected)
+//  }
+//
+//  it should "extract the correct rightsHolder" in {
+//    val expected = Seq("Eric Rights Holder").map(nameOnlyAgent)
+//    assert(extractor.rightsHolder(xml) === expected)
+//  }
+//
+//  it should "extract the correct subjects" in {
+//    val expected = Seq("Recital programs").map(nameOnlyConcept)
+//    assert(extractor.subject(xml) === expected)
+//  }
+//
+//  it should "extract the correct titles" in {
+//    val expected = Seq("Valrie Kantorski & Ann Pope")
+//    assert(extractor.title(xml) === expected)
+//  }
+//
+//  it should "extract the correct types" in {
+//    val expected = Seq("Image","Text")
+//    assert(extractor.`type`(xml) === expected)
+//  }
+//
+//  it should "create the correct DPLA URI" in {
+//    val expected = new URI("http://dp.la/api/items/130137a53d59ee27d7dab4e0078e1220")
+//    assert(extractor.dplaUri(xml) === expected)
+//  }
+//
+//  it should "extract the correct dataProvider" in {
+//    val expected = List(nameOnlyAgent("Bowling Green State University Libraries"))
+//    assert(extractor.dataProvider(xml) === expected)
+//  }
+//
+//  it should "extract the correct edmRights" in {
+//    val expected = List(new URI("http://rightsstatements.org/page/NoC-US/1.0/"))
+//    assert(extractor.edmRights(xml) === expected)
+//  }
+//
+//  it should "extract the correct isShownAt" in {
+//    val expected = List(uriOnlyWebResource(new URI("https://digitalgallery.bgsu.edu/collections/item/14058")))
+//    assert(extractor.isShownAt(xml) === expected)
+//  }
+//
+//  it should "extract the correct preview" in {
+//    val expected = List(uriOnlyWebResource(new URI("https://digitalgallery.bgsu.edu/files/thumbnails/26e197915e9107914faa33ac166ead5a.jpg")))
+//    assert(extractor.preview(xml) === expected)
+//  }
+//
+//  // Extent format helper
+//  it should "extract extent values from format" in {
+//    val xml: NodeSeq = <record><metadata>
+//      <format>1 x 2 x 3</format>
+//      <format>written</format>
+//    </metadata></record>
+//
+//    val expected = Seq("1 x 2 x 3")
+//    assert(extractor.extentFromFormat(Document(xml)) === expected)
+//  }
+}
