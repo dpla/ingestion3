c8f932cd64439081d3e928dca85a67b554c38cdb
Audrey Altman <audrey@dp.la>
2018-11-20 07:47:34 -0500

    more tests

--- src/main/scala/dpla/ingestion3/mappers/providers/VirginiaMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/VirginiaMapping.scala
@@ -25,115 +25,77 @@ class VirginiaMapping extends Mapping[NodeSeq] with XmlExtractor with IdMinter[N
 
   // TODO: What is persistent ID?
   override def getProviderId(implicit data: Document[NodeSeq]): String =
-    extractString(data \ "header" \ "identifier")
+    extractString(data \ "identifier")
       .getOrElse(throw new RuntimeException(s"No ID for record $data"))
-
-  // SourceResource mapping
-  override def alternateTitle(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \\ "alternative")
 
   // Only use the first isPartOf instance
   override def collection(data: Document[NodeSeq]): Seq[DcmiTypeCollection] =
-    extractStrings(data \ "metadata" \\ "isPartOf")
+    extractStrings(data \ "isPartOf")
       .map(nameOnlyCollection)
 
-  override def contributor(data: Document[NodeSeq]): Seq[EdmAgent] =
-    extractStrings(data \ "metadata" \\ "contributor")
-      .flatMap(_.splitAtDelimiter(";"))
-      .map(nameOnlyAgent)
-
   override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
-    extractStrings(data \ "metadata" \\ "creator")
-      .flatMap(_.splitAtDelimiter(";"))
+    extractStrings(data \ "creator")
       .map(nameOnlyAgent)
 
   override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
-    extractStrings(data \ "metadata" \\ "date")
-      .flatMap(_.splitAtDelimiter(";"))
+    extractStrings(data \ "created")
       .map(stringOnlyTimeSpan)
 
   override def description(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \\ "description")
+    extractStrings(data \ "description")
 
   override def extent(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \\ "extent") ++
-      extentFromFormat(data)
+    extractStrings(data \ "extent")
 
+  // TODO: apply block filters a la Ohio?
   override def format(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \\ "format")
-      .flatMap(_.splitAtDelimiter(";"))
-      .map(_.applyBlockFilter(
-        DigitalSurrogateBlockList.termList ++
-          FormatTypeValuesBlockList.termList ++
-          ExtentIdentificationList.termList))
-      .filter(_.nonEmpty)
+    extractStrings(data \ "medium")
 
   override def identifier(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \\ "identifier")
+    extractStrings(data \ "identifier")
 
   override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
-    extractStrings(data \ "metadata" \\ "language")
-      .flatMap(_.splitAtDelimiter(";"))
+    extractStrings(data \ "language")
       .map(nameOnlyConcept)
 
   override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
-    extractStrings(data \ "metadata" \\ "spatial")
-      .flatMap(_.split(";"))
+    extractStrings(data \ "spatial")
       .map(nameOnlyPlace)
 
   override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
-    extractStrings(data \ "metadata" \\ "publisher")
+    extractStrings(data \ "publisher")
       .map(nameOnlyAgent)
 
-  override def relation(data: Document[NodeSeq]): Seq[LiteralOrUri] =
-    extractStrings(data \ "metadata" \\ "relation")
-      .map(eitherStringOrUri)
-
+  // TODO: Is this the rights statement field?
   override def rights(data: Document[NodeSeq]): Seq[String] =
-    (data \ "metadata" \\ "rights").map(r => {
-      r.prefix match {
-        case "dc" => r.text
-        case _ => ""
-      }
-    }).filter(_.nonEmpty)
+    extractStrings(data \ "rights")
 
-  override def rightsHolder(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
-    extractStrings(data \ "metadata" \\ "rightsHolder")
-      .map(nameOnlyAgent)
-
+  // TODO: capitalize first char a la Ohio?
   override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
-    extractStrings(data \ "metadata" \\ "subject")
-      .flatMap(_.splitAtDelimiter(";"))
-      .map(_.capitalizeFirstChar)
+    extractStrings(data \ "subject")
       .map(nameOnlyConcept)
 
   override def title(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \\ "title")
+    extractStrings(data \ "title")
 
   override def `type`(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \\ "type")
+    extractStrings(data \ "type")
 
   // OreAggregation
   override def dplaUri(data: Document[NodeSeq]): URI = mintDplaItemUri(data)
 
   override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
-    extractStrings(data \ "metadata" \\ "dataProvider")
+    extractStrings(data \ "provenance")
       .map(nameOnlyAgent)
 
-  override def edmRights(data: Document[NodeSeq]): ZeroToMany[URI] = {
-    (data \ "metadata" \\ "rights").map(r => r.prefix match {
-      case "edm" => URI(r.text)
-    })
-  }
-
   override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
-    extractStrings(data \ "metadata" \\ "isShownAt")
+    extractStrings(data \ "isShownAt")
       .map(stringOnlyWebResource)
 
   override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = Utils.formatXml(data)
 
   override def preview(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
-    extractStrings(data \ "metadata" \\ "preview")
+    extractStrings(data \ "preview")
       .map(stringOnlyWebResource)
 
   override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = agent
@@ -142,20 +104,14 @@ class VirginiaMapping extends Mapping[NodeSeq] with XmlExtractor with IdMinter[N
     ("prehashId" -> buildProviderBaseId()(data)) ~ ("dplaId" -> mintDplaId(data))
 
   // Helper method
+  // TODO: correct URI?
   def agent = EdmAgent(
-    name = Some("Ohio Digital Network"),
-    uri = Some(URI("http://dp.la/api/contributor/ohio"))
+    name = Some("Digital Virginias"),
+    uri = Some(URI("http://dp.la/api/contributor/virginia"))
   )
 
-  /**
-    * Extracts values from format field and returns values that appear to be
-    * extent statements
-    * @param data
-    * @return
-    */
-  def extentFromFormat(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \\ "format")
-      .flatMap(_.splitAtDelimiter(";"))
-      .map(_.extractExtents)
-      .filter(_.nonEmpty)
+  // TODO: extentFromFormat a la Ohio?
+
+  // TODO: subType
+  // TODO: temporal
 }
