b0adfc5dad119b69a140cca9816d60e25145365d
Michael Della Bitta <mdellabitta@gmail.com>
2018-11-27 16:23:42 -0500

    WIP on new P2P mapping.

--- src/main/scala/dpla/ingestion3/mappers/providers/P2PMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/P2PMapping.scala
@@ -1,15 +1,15 @@
 package dpla.ingestion3.mappers.providers
 
-import dpla.ingestion3.mappers.utils.{Document, IdMinter, JsonExtractor, Mapping}
+import dpla.ingestion3.mappers.utils._
+import dpla.ingestion3.messages.IngestMessageTemplates
 import dpla.ingestion3.model.DplaMapData._
 import dpla.ingestion3.model.{EdmAgent, _}
-import dpla.ingestion3.utils.Utils
 import org.json4s.JValue
-import org.json4s.JsonAST.JString
 import org.json4s.JsonDSL._
-import org.json4s.jackson.JsonMethods._
 
-class P2PMapping() extends Mapping[JValue] with IdMinter[JValue] with JsonExtractor {
+import scala.xml.NodeSeq
+
+class P2PMapping() extends Mapping[NodeSeq] with XmlExtractor with IdMinter[NodeSeq] with IngestMessageTemplates {
 
   // ID minting functions
   override def useProviderName: Boolean = true
@@ -17,70 +17,114 @@ class P2PMapping() extends Mapping[JValue] with IdMinter[JValue] with JsonExtrac
   // Hard coded to prevent accidental changes to base ID
   override def getProviderName: String = "p2p"
 
-  override def getProviderId(implicit data: Document[JValue]): String =
-    (unwrap(data) \ "@graph").children.flatMap(elem => {
-      elem \ "@type" match {
-        case JString("ore:Aggregation") => extractString("@id")(elem)
-        case _ => None
-      }
-    }).headOption
-      .getOrElse(throw new RuntimeException(s"No ID for record: ${compact(data)}"))
+  override def getProviderId(implicit data: Document[NodeSeq]): String =
+    extractString(data \\ "header" \ "identifier")
+      .getOrElse(throw new RuntimeException(s"No ID for record $data"))
 
   // OreAggregation fields
-  override def dplaUri(data: Document[JValue]): ExactlyOne[URI] = mintDplaItemUri(data)
+  override def dplaUri(data: Document[NodeSeq]): ExactlyOne[URI] =
+    mintDplaItemUri(data)
 
-  override def dataProvider(data: Document[JValue]): ZeroToMany[EdmAgent] =
-    extractStringsDeep("edm:dataProvider")(data)
-      .map(nameOnlyAgent)
+  override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    for {
+      node <- data \ "mods" \ "note"
+      if node \@ "type" == "ownership"
+    } yield nameOnlyAgent(node.text.trim)
 
-  override def edmRights(data: Document[JValue]): ZeroToMany[URI] =
-    extractStrings(unwrap(data) \ "@graph" \ "edm:rights" \ "@id").map(URI)
 
-  override def isShownAt(data: Document[JValue]): ZeroToMany[EdmWebResource] =
-    extractStrings(unwrap(data) \ "@graph" \ "edm:isShownAt" \ "@id").map(stringOnlyWebResource)
+  override def edmRights(data: Document[NodeSeq]): ZeroToMany[URI] = ???
 
-  override def originalRecord(data: Document[JValue]): ExactlyOne[String] = Utils.formatJson(data)
+  override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] = ???
 
-  override def preview(data: Document[JValue]): ZeroToMany[EdmWebResource] =
-    extractStrings(unwrap(data) \ "@graph" \ "edm:preview" \ "@id").map(stringOnlyWebResource)
+  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = ???
 
-  override def provider(data: Document[JValue]): ExactlyOne[EdmAgent] = EdmAgent(
+  override def preview(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] = ???
+
+  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = EdmAgent(
     name = Some("Plains to Peaks Collective"),
     uri = Some(URI("http://dp.la/api/contributor/p2p"))
   )
 
-  override def sidecar(data: Document[JValue]): JValue =
+  override def sidecar(data: Document[NodeSeq]): JValue =
     ("prehashId", buildProviderBaseId()(data)) ~ ("dplaId", mintDplaId(data))
 
   // SourceResource
-  override def alternateTitle(data: Document[JValue]): ZeroToMany[String] =
-    extractStringsDeep("dcterm:alternative")(data)
+  override def alternateTitle(data: Document[NodeSeq]): ZeroToMany[String] = ???
 
-  override def contributor(data: Document[JValue]): ZeroToMany[EdmAgent] =
-    extractStringsDeep("dcterm:contributor")(data).map(nameOnlyAgent)
+  override def contributor(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    for {
+      name <- data \ "mods" \ "name"
+      if (name \ "role" \ "roleTerm").text.trim == "contributor"
+    } yield nameOnlyAgent((name \ "namePart").text.trim)
 
-  override def creator(data: Document[JValue]): ZeroToMany[EdmAgent] =
-    extractStringsDeep("dcterm:creator")(data).map(nameOnlyAgent)
+  override def creator(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    for {
+      name <- data \ "mods" \ "name"
+      if (name \ "role" \ "roleTerm").text.trim == "creator"
+    } yield nameOnlyAgent((name \ "namePart").text.trim)
 
-  override def date(data: Document[JValue]): ZeroToMany[EdmTimeSpan] =
-    extractStringsDeep("dc:date")(data).map(stringOnlyTimeSpan)
+  override def date(data: Document[NodeSeq]): ZeroToMany[EdmTimeSpan] =
+    extractStrings(data \ "mods" \ "originInfo" \ "dateCreated")
+      .map(stringOnlyTimeSpan)
 
-  override def description(data: Document[JValue]): ZeroToMany[String] =
-    extractStringsDeep("dcterm:description")(data)
+  override def description(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "mods" \ "abstract")
 
-  override def extent(data: Document[JValue]): ZeroToMany[String] =
-    extractStringsDeep("dcterm:extent")(data)
+  override def extent(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "mods" \ "phyiscialDescription" \ "extent")
 
-  override def identifier(data: Document[JValue]): ZeroToMany[String] =
-    extractStringsDeep("dcterm:identifier")(data)
+  override def identifier(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "recordInfo" \ "recordIdentifier")
 
-  override def language(data: Document[JValue]): ZeroToMany[SkosConcept] =
-    extractStringsDeep("dcterm:language")(data).map(nameOnlyConcept)
-
-  override def subject(data: Document[JValue]): ZeroToMany[SkosConcept] =
-    extractStringsDeep("dcterm:subject")(data)
+  override def language(data: Document[NodeSeq]): ZeroToMany[SkosConcept] =
+    extractStrings(data \ "mods" \ "language" \ "languageTerm")
       .map(nameOnlyConcept)
 
-  override def title(data: Document[JValue]): AtLeastOne[String] =
-    extractStringsDeep("dcterm:title")(data)
+  override def subject(data: Document[NodeSeq]): ZeroToMany[SkosConcept] =
+    for {
+      subjectNode <- data \ "mods" \ "subject"
+      subject <-
+        subjectNode \ "topic" ++
+          subjectNode \ "name" ++
+          subjectNode \ "genre"
+    } yield nameOnlyConcept(subject.text.trim)
+
+  override def title(data: Document[NodeSeq]): AtLeastOne[String] =
+    extractStrings(data \ "mods" \ "titleInfo" \ "title")
+
+  override def `type`(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "mods" \ "typeOfResource")
+
+  override def publisher(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    extractStrings(data \ "mods" \ "originInfo" \ "publisher")
+      .map(nameOnlyAgent)
+
+  override def format(data: Document[NodeSeq]): ZeroToMany[String] =
+    extractStrings(data \ "mods" \ "physicalDescription" \ "form")
+
+  override def place(data: Document[NodeSeq]): ZeroToMany[DplaPlace] =
+    extractStrings(data \ "mods" \ "subject" \ "geographic")
+      .map(nameOnlyPlace)
+
+  override def intermediateProvider(data: Document[NodeSeq]): ZeroToOne[EdmAgent] =
+    (for {
+      node <- data \ "mods" \ "note"
+      if node \@ "type" == "admin"
+    } yield nameOnlyAgent(node.text.trim)).headOption
+
+  override def relation(data: Document[NodeSeq]): ZeroToMany[LiteralOrUri] =
+    for {
+      relatedItem <- data \ "mods" \ "relatedItem"
+      if relatedItem \@ "type" == "series"
+      relation <- relatedItem \ "titleInfo" \ "title"
+    } yield Left(relation.text.trim)
+
+  override def collection(data: Document[NodeSeq]): ZeroToMany[DcmiTypeCollection] =
+    for {
+      relatedItem <- data \ "mods" \ "relatedItem"
+      if relatedItem \@ "type" == "host"
+      collectionTitle <- relatedItem \ "titleInfo" \ "title"
+    } yield nameOnlyCollection(collectionTitle.text.trim)
+
+
 }
