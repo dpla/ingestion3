26d725610a046aeedb5da73f83d7fa9620ebad33
Audrey Altman <audrey@dp.la>
2018-11-28 19:14:54 -0500

    initial mapping

--- src/main/scala/dpla/ingestion3/mappers/providers/MoMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/MoMapping.scala
@@ -1,13 +1,11 @@
 package dpla.ingestion3.mappers.providers
 
-import dpla.ingestion3.enrichments.normalizations.StringNormalizationUtils._
 import dpla.ingestion3.enrichments.normalizations.filters.ExtentIdentificationList
 import dpla.ingestion3.mappers.utils._
 import dpla.ingestion3.messages.IngestMessageTemplates
-import dpla.ingestion3.model.DplaMapData.{AtLeastOne, ExactlyOne, ZeroToMany, ZeroToOne}
+import dpla.ingestion3.model.DplaMapData.{AtLeastOne, ExactlyOne, ZeroToMany}
 import dpla.ingestion3.model._
 import dpla.ingestion3.utils.Utils
-import org.json4s
 import org.json4s.JsonDSL._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
@@ -17,8 +15,10 @@ class MoMapping extends JsonMapping with JsonExtractor with IdMinter[JValue] wit
   val formatBlockList: Set[String] = ExtentIdentificationList.termList
 
   // ID minting functions
+  // TODO: Should this be true or false?
   override def useProviderName: Boolean = true
 
+  // TODO: Should this be the same as provider short name?
   override def getProviderName: String = "mo"
 
   override def getProviderId(implicit data: Document[JValue]): String =
@@ -26,6 +26,7 @@ class MoMapping extends JsonMapping with JsonExtractor with IdMinter[JValue] wit
       .getOrElse(throw new RuntimeException(s"No ID for record: ${compact(data)}"))
 
   // OreAggregation
+
   override def dataProvider(data: Document[JValue]): ZeroToMany[EdmAgent] =
     extractStrings(unwrap(data) \ "dataProvider").map(nameOnlyAgent)
 
@@ -48,8 +49,52 @@ class MoMapping extends JsonMapping with JsonExtractor with IdMinter[JValue] wit
   override def sidecar(data: Document[JValue]): JValue =
     ("prehashId", buildProviderBaseId()(data)) ~ ("dplaId", mintDplaId(data))
 
+  // SourceResource
+
+  override def creator(data: Document[JValue]): ZeroToMany[EdmAgent] =
+    extractStrings(unwrap(data)  \ "sourceResource" \ "creator").map(nameOnlyAgent)
+
+  override def description(data: Document[JValue]): ZeroToMany[String] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "description")
+
+  override def format(data: Document[JValue]): ZeroToMany[String] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "format")
+
+  // TODO: Confirm with team that this is an appropriate mapping
+  override def genre(data: Document[JValue]): ZeroToMany[SkosConcept] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "specType").map(nameOnlyConcept)
+
+  override def identifier(data: Document[JValue]): ZeroToMany[String] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "identifier")
+
+  // TODO: Confirm with team that this is an appropriate mapping
+  // Initial analysis suggests that all languages have iso639_3 values
+  override def language(data: Document[JValue]): ZeroToMany[SkosConcept] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "language" \ "iso639_3").map(nameOnlyConcept)
+
+  override def rights(data: Document[JValue]): AtLeastOne[String] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "rights")
+
+  override def subject(data: Document[JValue]): ZeroToMany[SkosConcept] =
+    extractStrings(unwrap(data)  \ "sourceResource" \ "subject" \ "name").map(nameOnlyConcept)
+
+  override def temporal(data: Document[JValue]): ZeroToMany[EdmTimeSpan] =
+     extractDate(unwrap(data) \ "sourceResource" \ "temporal")
+
+  override def title(data: Document[JValue]): AtLeastOne[String] =
+    extractStrings(unwrap(data) \ "sourceResource" \ "title")
+
   def agent = EdmAgent(
     name = Some("Missouri Hub"),
     uri = Some(URI("http://dp.la/api/contributor/missouri-hub"))
   )
+
+  def extractDate(date: JValue): ZeroToMany[EdmTimeSpan] = {
+    iterify(date).children.map(d =>
+      EdmTimeSpan(
+        begin = extractString(d \ "start"),
+        end = extractString(d \ "end"),
+        originalSourceDate = extractString(d \ "displayDate")
+      ))
+  }
 }
--- src/test/scala/dpla/ingestion3/mappers/providers/MoMappingTest.scala
+++ src/test/scala/dpla/ingestion3/mappers/providers/MoMappingTest.scala
@@ -2,7 +2,7 @@
 package dpla.ingestion3.mappers.providers
 
 import dpla.ingestion3.mappers.utils.Document
-import dpla.ingestion3.model.{DcmiTypeCollection, _}
+import dpla.ingestion3.model._
 import dpla.ingestion3.utils.FlatFileIO
 import org.json4s.JsonAST.JValue
 import org.json4s.jackson.JsonMethods._
@@ -24,7 +24,7 @@ class MoMappingTest extends FlatSpec with BeforeAndAfter {
     assert(extractor.getProviderName === "mo")
   }
 
-  it should "extract the correct identifier" in {
+  it should "extract the correct providerId" in {
     val expected = "missouri--urn:data.mohistory.org:msu_all:oai:digitalcollections.missouristate.edu:Hennicke/94"
     assert(extractor.getProviderId(json) === expected)
   }
@@ -57,5 +57,69 @@ class MoMappingTest extends FlatSpec with BeforeAndAfter {
   it should "extract the correct object" in {
     val expected = List(stringOnlyWebResource("http://digitalcollections.missouristate.edu/utils/getthumbnail/collection/Hennicke/id/94"))
     assert(extractor.`object`(json) === expected)
+  }
+
+  // creator
+  it should "extract the correct creator" in {
+    val expected = List("Leo. Feist","Herman Darewski Music").map(nameOnlyAgent)
+    assert(extractor.creator(json) === expected)
+  }
+
+  // description
+  it should "extract the correct description" in {
+    val expected = List("Six students posing on a bench.")
+    assert(extractor.description(json) === expected)
+  }
+
+  // format
+  it should "extract the correct format" in {
+    val expected = List("Book")
+    assert(extractor.format(json) === expected)
+  }
+
+  // genre
+  it should "extract the correct genre from specType field" in {
+    val expected = List("Photograph/Pictorial Works").map(nameOnlyConcept)
+    assert(extractor.genre(json) === expected)
+  }
+
+  // identifier
+  it should "extract the correct identifier" in {
+    val expected = List("http://digitalcollections.missouristate.edu/cdm/ref/collection/Hennicke/id/94")
+    assert(extractor.identifier(json) === expected)
+  }
+
+  // language
+  it should "extract the correct language" in {
+    val expected = List("eng").map(nameOnlyConcept)
+    assert(extractor.language(json) === expected)
+  }
+
+  // rights
+  it should "extract the correct rights" in {
+    val expected = List("Use of digital images found on this website is permitted for private or personal use only.")
+    assert(extractor.rights(json) === expected)
+  }
+
+  // subject
+  it should "extract the correct subjects" in {
+    val expected = List("Schools--Missouri--Springfield", " Alternative education", " Springfield (Mo.)", "")
+      .map(nameOnlyConcept)
+    assert(extractor.subject(json) === expected)
+  }
+
+  // temporal
+  it should "extract the correct temporal with display, begin and end" in {
+    val expected = List(
+      EdmTimeSpan(originalSourceDate = Some("1940-05")),
+      EdmTimeSpan(originalSourceDate = Some("1991/1995"), begin = Some("1991"), end = Some("1995"))
+    )
+    assert(extractor.temporal(json) === expected)
+  }
+
+  // title
+  it should "extract the correct title" in {
+    val expected = List("Photograph of students posing on bench")
+    assert(extractor.title(json) === expected)
   }
 }
