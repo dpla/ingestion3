4a7dbe80ae4daa95090c22eb9943c7caebffdd82
Audrey Altman <audrey@dp.la>
2018-12-11 17:34:57 -0500

    remove unused import

--- src/main/scala/dpla/ingestion3/executors/EnrichExecutor.scala
+++ src/main/scala/dpla/ingestion3/executors/EnrichExecutor.scala
@@ -17,7 +17,6 @@ import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.catalyst.encoders.{ExpressionEncoder, RowEncoder}
-import org.apache.spark.sql.functions.col
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.util.LongAccumulator
 
