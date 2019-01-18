c62974b396a182c5bb4de5bddfac4fd9e5b83162
Scott Williams <scott@dp.la>
2019-01-18 12:19:00 -0500

    Remove existing date enrichment and replace with simplified version that only extracts begin and end dates from yyyy and yyyy-yyyy patterns Update tests Enable date enrichment in EnrichmentDriver

--- src/test/scala/dpla/ingestion3/enrichments/DateBuilderTest.scala
+++ src/test/scala/dpla/ingestion3/enrichments/DateBuilderTest.scala
@@ -9,204 +9,25 @@ class DateBuilderTest extends FlatSpec with BeforeAndAfter with Matchers with Ch
 
   val dateBuilder = new DateBuilder()
 
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy-mm-dd" in {
-    val dateStr = "2015-05-01"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some(dateStr),
-      begin = Some(dateStr),
-      end = Some(dateStr)
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy/mm/dd" in {
-    val dateStr = "2015/05/01"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05-01"),
-      begin = Some("2015-05-01"),
-      end = Some("2015-05-01")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy mm dd" in {
-    val dateStr = "2015 05 01"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05-01"),
-      begin = Some("2015-05-01"),
-      end = Some("2015-05-01")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
+   it should "create a valid EdmTimeSpan with begin and end dates for 2015" in {
+     val dateStr = Some("2015")
+     val expectedDate = EdmTimeSpan(
+       originalSourceDate = dateStr,
+       prefLabel = dateStr ,
+       begin = dateStr ,
+       end = dateStr
+     )
+     assert(dateBuilder.generateBeginEnd(dateStr) === expectedDate)
+   }
 
-  // yyyy-MMM-dd
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy-MMM-dd" in {
-    val dateStr = "2015-May-01"
+  it should "create a valid EdmTimeSpan with begin and end dates for 2015-2017" in {
+    val dateStr = Some("2015-2017")
     val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05-01"),
-      begin = Some("2015-05-01"),
-      end = Some("2015-05-01")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy/MMM/dd" in {
-    val dateStr = "2015/May/01"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05-01"),
-      begin = Some("2015-05-01"),
-      end = Some("2015-05-01")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy MMM dd" in {
-    val dateStr = "2015 May 01"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05-01"),
-      begin = Some("2015-05-01"),
-      end = Some("2015-05-01")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  // MMM-yyyy
-  it should "create a valid EdmTimeSpan with begin and end dates for MMM-yyyy" in {
-    val dateStr = "May-2015"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05"),
-      begin = Some("2015-05"),
-      end = Some("2015-05")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  it should "create a valid EdmTimeSpan with begin and end dates for MMM/yyyy" in {
-    val dateStr = "May/2015"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05"),
-      begin = Some("2015-05"),
-      end = Some("2015-05")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  it should "create a valid EdmTimeSpan with begin and end dates for MMM yyyy" in {
-    val dateStr = "May 2015"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-05"),
-      begin = Some("2015-05"),
-      end = Some("2015-05")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  // yyyy MMM
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy-MMM" in {
-    val dateStr = "2015-Mar"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-03"),
-      begin = Some("2015-03"),
-      end = Some("2015-03")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy/MMM" in {
-    val dateStr = "2015/Mar"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-03"),
-      begin = Some("2015-03"),
-      end = Some("2015-03")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy MMM" in {
-    val dateStr = "2015 Mar"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-03"),
-      begin = Some("2015-03"),
-      end = Some("2015-03")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  // yyyy MM
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy-MM" in {
-    val dateStr = "2015-03"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-03"),
-      begin = Some("2015-03"),
-      end = Some("2015-03")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy/MM" in {
-    val dateStr = "2015/03"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-03"),
-      begin = Some("2015-03"),
-      end = Some("2015-03")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy MM" in {
-    val dateStr = "2015 03"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015-03"),
-      begin = Some("2015-03"),
-      end = Some("2015-03")
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  // yyyy
-  it should "create a valid EdmTimeSpan with begin and end dates for yyyy" in {
-    val dateStr = "2015"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = Some("2015"),
+      originalSourceDate = dateStr,
+      prefLabel = dateStr,
       begin = Some("2015"),
-      end = Some("2015")
+      end = Some("2017")
     )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-
-  // Anti-tests
-  // 1993-1994
-  it should "do nothing with 1993-1994; " in {
-    val dateStr = "1993-1994; "
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = None,
-      begin = None,
-      end = None
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
-  }
-  // 1975/1985
-  it should "do nothing with 1975/1985" in {
-    val dateStr = "1975/1985"
-    val expectedDate = EdmTimeSpan(
-      originalSourceDate = Some(dateStr),
-      prefLabel = None,
-      begin = None,
-      end = None
-    )
-    assert(dateBuilder.buildEdmTimeSpan(dateStr) === expectedDate)
+    assert(dateBuilder.generateBeginEnd(dateStr) === expectedDate)
   }
 }
