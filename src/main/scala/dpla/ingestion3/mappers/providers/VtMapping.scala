04abbcc31e44a915560c2f40d58106cc3a82270e
AudreyAltman <audreykaltman@gmail.com>
2019-01-31 18:07:58 -0500

    draft vt mapping

--- src/main/scala/dpla/ingestion3/mappers/providers/VtMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/VtMapping.scala
@@ -2,28 +2,102 @@ package dpla.ingestion3.mappers.providers
 
 import dpla.ingestion3.mappers.utils.{Document, XmlExtractor, XmlMapping}
 import dpla.ingestion3.messages.IngestMessageTemplates
-import dpla.ingestion3.model.DplaMapData.{ExactlyOne, ZeroToMany, ZeroToOne}
+import dpla.ingestion3.model.DplaMapData._
 import dpla.ingestion3.model._
-import org.json4s
+import dpla.ingestion3.utils.Utils
+import org.json4s.JValue
+import org.json4s.JsonDSL._
 
 import scala.xml._
 
 class VtMapping extends XmlMapping with XmlExtractor with IngestMessageTemplates {
 
-  override def dplaUri(data: Document[NodeSeq]): ZeroToOne[URI] = ???
+  // TODO: There doesn't appear to be any thumbnails?
 
-  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = ???
+  override def useProviderName: Boolean = false
 
-  override def useProviderName: Boolean = ???
+  override def getProviderName(): String = "vt"
 
-  override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] = ???
+  // TODO: Confirm with provider that this is correct.  Some records have multiple identifiers.
+  override def originalId(implicit data: Document[NodeSeq]): ZeroToOne[String] =
+    extractStrings(data \ "identifier").headOption
 
-  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = ???
+  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] =
+    Utils.formatXml(data)
 
-  override def sidecar(data: Document[NodeSeq]): json4s.JValue = ???
+  override def dplaUri(data: Document[NodeSeq]): ZeroToOne[URI] =
+    mintDplaItemUri(data)
 
-  override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] = ???
+  // TODO: Not all identifiers are URLs, and some records have multiple identifiers.
+  //  Check to see that identifier is URL?
+  override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
+    extractStrings(data \ "identifier")
+      .map(stringOnlyWebResource)
 
-  override def originalId(implicit data: Document[NodeSeq]): ZeroToOne[String] = ???
+  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = agent
 
+  // TODO: I don't think source is the correct field to map, but there is no alternative.
+  //  It contains name/address of archives and collection names.
+  //  It is only present in about half the records and some records have more than one.
+  override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
+    extractStrings(data \ "source")
+      .map(nameOnlyAgent)
+
+  override def contributor(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "contributor")
+      .map(nameOnlyAgent)
+
+  override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "creator")
+      .map(nameOnlyAgent)
+
+  override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
+    extractStrings(data \ "date")
+      .map(stringOnlyTimeSpan)
+
+  override def description(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "description")
+
+  override def format(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "format")
+
+  override def identifier(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "identifier")
+
+  override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \ "language")
+      .map(nameOnlyConcept)
+
+  override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
+    extractStrings(data \ "coverage")
+      .map(nameOnlyPlace)
+
+  override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
+    extractStrings(data \ "publisher")
+      .map(nameOnlyAgent)
+
+  override def relation(data: Document[NodeSeq]): ZeroToMany[LiteralOrUri] =
+    extractStrings(data \ "relation")
+      .map(eitherStringOrUri)
+
+  override def rights(data: Document[NodeSeq]): AtLeastOne[String] =
+    extractStrings(data \ "rights")
+
+  override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
+    extractStrings(data \ "subject")
+      .map(nameOnlyConcept)
+
+  override def title(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "title")
+
+  override def `type`(data: Document[NodeSeq]): Seq[String] =
+    extractStrings(data \ "type")
+
+  override def sidecar(data: Document[NodeSeq]): JValue =
+    ("prehashId" -> buildProviderBaseId()(data)) ~ ("dplaId" -> mintDplaId(data))
+
+  def agent = EdmAgent(
+    name = Some("Green Mountain Digital Archive"),
+    uri = Some(URI("http://dp.la/api/contributor/vt"))
+  )
 }
---
+++ src/test/scala/dpla/ingestion3/mappers/providers/VtMappingTest.scala
@@ -0,0 +1,110 @@
+package dpla.ingestion3.mappers.providers
+
+import dpla.ingestion3.mappers.utils.Document
+import dpla.ingestion3.messages.{IngestMessage, MessageCollector}
+import dpla.ingestion3.utils.FlatFileIO
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import dpla.ingestion3.model._
+
+import scala.xml.{NodeSeq, XML}
+
+class VtMappingTest extends FlatSpec with BeforeAndAfter {
+
+  implicit val msgCollector: MessageCollector[IngestMessage] = new MessageCollector[IngestMessage]
+  val shortName = "vt"
+  val xmlString: String = new FlatFileIO().readFileAsString("/vt.xml")
+  val xml: Document[NodeSeq] = Document(XML.loadString(xmlString))
+  val extractor = new VtMapping
+
+  it should "not use the provider shortname in minting IDs "in
+    assert(!extractor.useProviderName)
+
+  it should "extract the correct original identifier " in
+    assert(extractor.originalId(xml) === Some("http://archive.org/details/hhfbc-c7"))
+
+  it should "create the correct DPLA URI" in {
+    val expected = Some(new URI("http://dp.la/api/items/9df2bbb82c4434954a12454414ca4174"))
+    assert(extractor.dplaUri(xml) === expected)
+  }
+
+  it should "extract the correct contributors" in {
+    val expected = Seq("Wilson, Douglas B.;").map(nameOnlyAgent)
+    assert(extractor.contributor(xml) == expected)
+  }
+
+  it should "extract the correct creators" in {
+    val expected = Seq("Flanders, Helen Hartness, 1890-1972").map(nameOnlyAgent)
+    assert(extractor.creator(xml) == expected)
+  }
+
+  it should "extract the correct dates" in {
+    val expected = Seq("1948").map(stringOnlyTimeSpan)
+    assert(extractor.date(xml) === expected)
+  }
+
+  it should "extract the correct description" in
+    assert(extractor.description(xml) == Seq("Box 1, Cylinder 7"))
+
+  it should "extract the correct format" in {
+    val expected = Seq("Archive BitTorrent", "JPEG Thumb", "Metadata", "Spectrogram")
+    assert(extractor.format(xml) === expected)
+  }
+
+  it should "extract the correct identifiers" in {
+    val expected = Seq("http://archive.org/details/hhfbc-c7", "RcXII_19_1921")
+    assert(extractor.identifier(xml) === expected)
+  }
+
+  it should "extract the correct language" in {
+    val expected = Seq("English").map(nameOnlyConcept)
+    assert(extractor.language(xml) === expected)
+  }
+
+  it should "extract the correct place" in {
+    val expected = Seq("Shrewsbury (Vt.)").map(nameOnlyPlace)
+    assert(extractor.place(xml) === expected)
+  }
+
+  it should "extract the correct publishers" in {
+    val expected = Seq("C. W. Hughes & Co., Inc., Mechanicville, N. Y.").map(nameOnlyAgent)
+    assert(extractor.publisher(xml) === expected)
+  }
+
+  it should "extract the correct relations" in {
+    val expected = Seq("Original from the Rodney Homeister Peterson Photographs.").map(eitherStringOrUri)
+    assert(extractor.relation(xml) ==  expected)
+  }
+
+  it should "extract the correct rights" in {
+    val expected = Seq("For questions or information about the rights for this image, please contact Special Collections & Archives, Middlebury College at specialcollections@middlebury.edu")
+    assert(extractor.rights(xml) == expected)
+  }
+
+  it should "extract the correct subjects" in {
+    val expected = Seq("Helen Hartness Flanders", "Ballads", "Burlington (Vt.)").map(nameOnlyConcept)
+    assert(extractor.subject(xml) === expected)
+  }
+
+  it should "extract the correct types" in {
+    val expected = Seq("Text")
+    assert(extractor.`type`(xml) === expected)
+  }
+
+  it should "extract the correct titles" in {
+    val expected = Seq("Cylinder 7 - Flanders Ballad Collection IRENE")
+    assert(extractor.title(xml) === expected)
+  }
+
+  it should "extract the correct dataProvider" in {
+    val expected = List(nameOnlyAgent("Middlebury College Special Collections"))
+    assert(extractor.dataProvider(xml) === expected)
+  }
+
+  it should "extract the correct isShownAt" in {
+    val expected = List(
+      uriOnlyWebResource(new URI("http://archive.org/details/hhfbc-c7")),
+      uriOnlyWebResource(new URI("RcXII_19_1921"))
+    )
+    assert(extractor.isShownAt(xml) === expected)
+  }
+}
