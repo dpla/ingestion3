44e91165d458ee5fca567248aa0a6fc95604d8b3
Scott Williams <scott@dp.la>
2019-09-03 13:40:21 -0400

    PA revisions

--- src/main/scala/dpla/ingestion3/mappers/providers/PaMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/PaMapping.scala
@@ -19,14 +19,14 @@ class PaMapping extends XmlMapping with XmlExtractor
   // getProviderName is not implemented here because useProviderName is false
 
   override def originalId(implicit data: Document[NodeSeq]): ZeroToOne[String] =
-    extractString(data \ "header" \ "identifier")
+    extractString(data \ "header" \ "identifier").map(_.trim)
 
   // SourceResource mapping
   override def alternateTitle(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \ "alternative")
+    extractStrings(metadataRoot(data) \ "alternative")
 
   override def collection(data: Document[NodeSeq]): Seq[DcmiTypeCollection] =
-    extractStrings(data \ "metadata" \ "isPartOf")
+    extractStrings(metadataRoot(data) \ "isPartOf")
       .map(nameOnlyCollection)
 
   override def contributor(data: Document[NodeSeq]): Seq[EdmAgent] =
@@ -34,56 +34,56 @@ class PaMapping extends XmlMapping with XmlExtractor
       .map(nameOnlyAgent)
 
   override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
-    extractStrings(data \ "metadata" \ "creator")
+    extractStrings(metadataRoot(data) \ "creator")
       .map(nameOnlyAgent)
 
   override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
-    extractStrings(data \ "metadata" \ "date")
+    extractStrings(metadataRoot(data) \ "date")
       .map(stringOnlyTimeSpan)
 
   override def description(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \ "description")
+    extractStrings(metadataRoot(data) \ "description")
 
   override def extent(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \ "extent")
+    extractStrings(metadataRoot(data) \ "extent")
 
   override def format(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \ "format")
+    extractStrings(metadataRoot(data) \ "format")
       .filterNot(isDcmiType)
 
   override def identifier(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \ "identifier")
+    extractStrings(metadataRoot(data) \ "identifier")
 
   override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
     extractStrings(data \ "metadata" \ "language")
       .map(nameOnlyConcept)
 
   override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
-    extractStrings(data \ "metadata" \ "spatial")
+    extractStrings(metadataRoot(data) \ "spatial")
       .map(nameOnlyPlace)
 
   override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
-    extractStrings(data \ "metadata" \ "publisher")
+    extractStrings(metadataRoot(data) \ "publisher")
       .map(nameOnlyAgent)
 
   override def relation(data: Document[NodeSeq]): Seq[LiteralOrUri] =
-    extractStrings(data \ "metadata" \ "relation")
+    extractStrings(metadataRoot(data) \ "relation")
       .map(eitherStringOrUri)
 
   override def replacedBy(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \ "isReplacedBy")
+    extractStrings(metadataRoot(data) \ "isReplacedBy")
 
   override def replaces(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "metadata" \ "replaces")
+    extractStrings(metadataRoot(data) \ "replaces")
 
   override def rightsHolder(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
-    extractStrings(data \ "metadata" \ "rightsHolder")
+    extractStrings(metadataRoot(data) \ "rightsHolder")
       .map(nameOnlyAgent)
 
   // Label    DPLA Field    PA Field
   // Rights	  dc:rights     dcterms:rights
   override def rights(data: Document[NodeSeq]): Seq[String] =
-    (data \ "metadata" \ "rights").flatMap(r => {
+    (metadataRoot(data) \ "rights").flatMap(r => {
       r.prefix match {
         case "dcterms" => Option(r.text)
         case _ => None
@@ -91,46 +91,46 @@ class PaMapping extends XmlMapping with XmlExtractor
     })
 
   override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
-    extractStrings(data \ "metadata" \ "subject")
+    extractStrings(metadataRoot(data) \ "subject")
       .map(nameOnlyConcept)
 
   override def temporal(data: Document[NodeSeq]): ZeroToMany[EdmTimeSpan] =
-    extractStrings(data \ "metadata" \ "temporal")
+    extractStrings(metadataRoot(data) \ "temporal")
       .map(stringOnlyTimeSpan)
 
   override def title(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \ "title")
+    extractStrings(metadataRoot(data) \ "title")
 
   override def `type`(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(data \ "metadata" \\ "type")
+    extractStrings(metadataRoot(data) \ "type")
 
   // OreAggregation
   override def dplaUri(data: Document[NodeSeq]): ZeroToOne[URI] = mintDplaItemUri(data)
 
   override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] =
-    extractStrings(data \ "metadata" \ "dataProvider")
+    extractStrings(metadataRoot(data) \ "dataProvider")
       .map(nameOnlyAgent)
 
   //  Label             DPLA Field    PA Field
   //  Rights Statement	edm:rights		edm:rights
   override def edmRights(data: Document[NodeSeq]): ZeroToMany[URI] =
-    (data \ "metadata" \ "rights").flatMap(r => r.prefix match {
+    (metadataRoot(data) \ "rights").flatMap(r => r.prefix match {
       case "edm" => Some(URI(r.text))
       case _ => None
     })
 
   override def intermediateProvider(data: Document[NodeSeq]): ZeroToOne[EdmAgent] =
-    extractStrings(data \ "metadata" \ "intermediateProvider")
+    extractStrings(metadataRoot(data) \ "intermediateProvider")
       .map(nameOnlyAgent).headOption
 
   override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
-    extractStrings(data \ "metadata" \ "isShownAt")
+    extractStrings(metadataRoot(data) \ "isShownAt")
       .map(stringOnlyWebResource)
 
   override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = Utils.formatXml(data)
 
   override def preview(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
-    extractStrings(data \ "metadata" \ "preview")
+    extractStrings(metadataRoot(data) \ "preview")
       .map(stringOnlyWebResource)
 
   override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = EdmAgent(
@@ -151,10 +151,12 @@ class PaMapping extends XmlMapping with XmlExtractor
     */
 
   def thumbnail(implicit data: Document[NodeSeq]): ZeroToOne[EdmWebResource] = {
-    val ids = extractStrings(data \ "metadata" \\ "identifier")
+    val ids = extractStrings(metadataRoot(data) \ "identifier")
     if (ids.size > 2)
       Option(uriOnlyWebResource(URI(ids.last)))
     else
       None
   }
+
+  def metadataRoot(data: Document[NodeSeq]): NodeSeq = data \ "metadata" \ "dc"
 }
