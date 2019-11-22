5d6f141a08c487c5de9cfde6bb3954c875220c42
Scott Williams <scott@dp.la>
2019-11-22 14:05:03 -0500

    Initial complete working draft of Portal to Texas History mapping and tests

--- src/main/scala/dpla/ingestion3/profiles/ProviderProfiles.scala
+++ src/main/scala/dpla/ingestion3/profiles/ProviderProfiles.scala
@@ -325,7 +325,6 @@ class TnProfile extends XmlProfile {
   type Mapping = TnMapping
 
   override def getHarvester = classOf[OaiHarvester]
-
   override def getMapping = new TnMapping
 }
 
@@ -336,7 +335,6 @@ class TxProfile extends XmlProfile {
   type Mapping = TnMapping
 
   override def getHarvester = classOf[OaiHarvester]
-
   override def getMapping = new TxMapping
 }
 
