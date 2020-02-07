4be46f4d3ea687f900a08c50e43ceb7fa6ef34da
Scott Williams <scott@dp.la>
2020-02-07 13:07:26 -0500

    Replace open/close quotes with neutal double quotes Replace literal with URI

--- src/main/scala/dpla/ingestion3/enrichments/TaggingUtils.scala
+++ src/main/scala/dpla/ingestion3/enrichments/TaggingUtils.scala
@@ -11,15 +11,15 @@ object TaggingUtils {
       */
     lazy val applyPanAmTags: Option[URI] = {
       // tag value to apply
-      val panAmTag = URI("PanAm")
+      val panAmTag = URI("http://dp.la/tags/panam")
 
       // Values for which the PanAm tag will be applied
       // Note: In case of a string that looks like it should match but doesn't,
       //       those are left/right double quotes and not \".
       val taggingValues = Seq(
-        "This item was digitized as part of the “Cleared to Land” project, supported by a grant from the National Historical Publications & Records Commission (NHPRC).",
+        "This item was digitized as part of the \"Cleared to Land\" project, supported by a grant from the National Historical Publications & Records Commission (NHPRC).",
 
-        "This item was digitized as part of the “Digitizing the 'World's Most Experienced Airline'” project, supported by a grant from the Council on Library and Information Resources (CLIR). The grant program is made possible by funding from The Andrew W. Mellon Foundation."
+        "This item was digitized as part of the \"Digitizing the 'World's Most Experienced Airline'\" project, supported by a grant from the Council on Library and Information Resources (CLIR). The grant program is made possible by funding from The Andrew W. Mellon Foundation."
       )
 
       if (taggingValues.contains(value)) {
