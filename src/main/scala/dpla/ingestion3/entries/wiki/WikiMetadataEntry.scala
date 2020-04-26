25bc691101a116f870511487c13abf487943668f
Scott Williams <scott@dp.la>
2020-04-25 21:49:01 -0400

    Add Executor and Entry

---
+++ src/main/scala/dpla/ingestion3/entries/wiki/WikiMetadataEntry.scala
@@ -0,0 +1,56 @@
+package dpla.ingestion3.entries.wiki
+
+import dpla.ingestion3.confs.CmdArgs
+import dpla.ingestion3.executors.WikiMetadataExecutor
+import dpla.ingestion3.utils.Utils
+import org.apache.spark.SparkConf
+
+object WikiMetadataEntry extends WikiMetadataExecutor {
+  /**
+    * Driver for reading DplaMapData records (mapped or enriched) and generating
+    * Wikidata markup as a part of a JSON record.
+    *
+    * The metadata/wikimedia markup can be ingested to Wikimedia Commons
+    * The
+    *
+    * Expects three parameters:
+    * 1) a path to the enriched data
+    * 2) a path to output the JSON data
+    * 3) provider short name (e.g. 'mdl', 'cdl', 'harvard')
+    * 4) spark master (optional parameter that overrides a --master param submitted
+    *    via spark-submit
+    *
+    * Usage
+    * -----
+    * To invoke via sbt:
+    * sbt "run-main dpla.ingestion3.WikiMetadataEntry
+    *       --input=/input/path/to/enriched/
+    *       --output=/output/path/to/wikimedia/
+    *       --name=shortName"
+    *       --sparkMaster=local[*]
+    */
+    def main(args: Array[String]): Unit = {
+
+      // Read in command line args.
+      val cmdArgs = new CmdArgs(args)
+
+      val dataIn = cmdArgs.getInput
+      val dataOut = cmdArgs.getOutput
+      val shortName = cmdArgs.getProviderName
+      val sparkMaster: Option[String] = cmdArgs.getSparkMaster
+
+      val baseConf =
+        new SparkConf()
+          .setAppName(s"Wiki Metadata Export: $shortName")
+
+      val sparkConf = sparkMaster match {
+        case Some(m) => baseConf.setMaster(m)
+        case None => baseConf
+      }
+
+      val logger = Utils.createLogger("wiki")
+
+      executeWikiMetadata(sparkConf, dataIn, dataOut, shortName, logger)
+    }
+}
+
---
+++ src/main/scala/dpla/ingestion3/executors/WikiMetadataExecutor.scala
@@ -0,0 +1,124 @@
+package dpla.ingestion3.executors
+
+import java.time.LocalDateTime
+
+import com.databricks.spark.avro._
+import dpla.ingestion3.dataStorage.OutputHelper
+import dpla.ingestion3.model.{ModelConverter, getDplaId, wikiRecord}
+import dpla.ingestion3.utils.FileLoader
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.util.{Failure, Success}
+
+case class WikiMetadata(dplaId: String, wikiMarkup: String)
+
+trait WikiMetadataExecutor extends Serializable with FileLoader {
+
+  /**
+    * Generate Wiki metadata JSON files from AVRO file
+    * @param sparkConf  Spark configuration
+    * @param dataIn     Data to transform into Wiki metadata
+    * @param dataOut    Location to save Wikimedia metadata
+    * @param shortName  Provider shortname
+    * @param logger     Logger object
+    */
+  def executeWikiMetadata(sparkConf: SparkConf,
+                   dataIn: String,
+                   dataOut: String,
+                   shortName: String,
+                   logger: Logger): String = {
+
+    // This start time is used for documentation and output file naming.
+    val startDateTime: LocalDateTime = LocalDateTime.now
+
+    val outputHelper: OutputHelper =
+      new OutputHelper(dataOut, shortName, "wiki", startDateTime)
+
+    val outputPath: String = outputHelper.activityPath
+
+    logger.info("Starting Wiki export")
+
+    val spark = SparkSession
+      .builder()
+      .config(sparkConf)
+      .getOrCreate()
+
+    import spark.implicits._
+    val sc = spark.sparkContext
+
+    val enrichedRows: DataFrame = spark.read.avro(dataIn)
+
+    // Read in Wikidata entity mapping resource files
+    val institutions =
+
+    enrichedRows.foreach(
+      row => {
+        val record = ModelConverter.toModel(row)
+        val dplaId = getDplaId(record)
+        val wikiMarkup = wikiRecord(record)
+        val path = getWikiPath(dplaId)
+        println(
+          s"""
+            | Output path: $path
+             Output file: ${}
+             Wikimarkup: ${wikiMarkup}
+          """.stripMargin)
+
+        // TODO write files out
+      }
+    )
+
+    // val wikiCount = wikiRecords.count
+
+    // This should always write out as #text() because if we use #json() then the
+    // data will be written out inside a JSON object (e.g. {'value': <doc>}) which is
+    // invalid for our use
+
+    // FIXME do not write out as text files need to
+    // indexRecords.write.text(outputPath)
+    // wikiRecords.toDF()
+
+    // println(wikiRecords.count())
+
+    //    wikiRecords.toDF.map(
+//      wikiRecord => {
+
+//      }
+//    )
+
+    // Create and write manifest.
+
+    val manifestOpts: Map[String, String] = Map(
+      "Activity" -> "Wiki",
+      "Provider" -> shortName,
+      "Record count" -> "TBD",
+      "Input" -> dataIn
+    )
+
+    outputHelper.writeManifest(manifestOpts) match {
+      case Success(s) => logger.info(s"Manifest written to $s")
+      case Failure(f) => logger.warn(s"Manifest failed to write: $f")
+    }
+
+    sc.stop()
+
+    logger.info("Wiki export complete")
+
+    // Return output path of wiki files.
+    outputPath
+  }
+
+  /**
+    *
+    * @param str
+    * @return
+    */
+  def getWikiPath(str: String) = {
+    s"{${str(0)}/${str(1)}/${str(2)}/${str(3)}/$str/}"
+  }
+
+
+}
+
