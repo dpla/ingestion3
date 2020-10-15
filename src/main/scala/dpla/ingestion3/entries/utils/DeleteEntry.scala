ebacdf90f29543f7ad4b0d45eaa24ebd2c01b271
scott <scott.brandon.williams@gmail.com>
2020-10-15 13:54:27 -0400

    Utility for deleting DPLAs and generating new JSONL (#393)

---
+++ src/main/scala/dpla/ingestion3/entries/utils/DeleteEntry.scala
@@ -0,0 +1,67 @@
+package dpla.ingestion3.entries.utils
+
+import dpla.ingestion3.confs.CmdArgs
+import dpla.ingestion3.dataStorage.InputHelper
+import dpla.ingestion3.executors.DeleteExecutor
+import dpla.ingestion3.utils.Utils
+import org.apache.spark.SparkConf
+
+/**
+  * Driver for reading DplaMapData records (mapped or enriched), deleting specific IDs
+  * and generating JSONL text, which can be bulk loaded into a DPLA Ingestion1 index, in
+  * Elasticsearch 0.90
+  *
+  * Expects three parameters:
+  * 1) a path to the enriched data
+  * 2) a path to output the jsonl data
+  * 3) provider short name (e.g. 'mdl', 'cdl', 'harvard')
+  * 4) spark master (optional parameter that overrides a --master param submitted
+  *    via spark-submit
+  * 5) a comma separated list of DPLA IDs to delete
+  *
+  * Usage
+  * -----
+  * To invoke via sbt:
+  * sbt "run-main dpla.ingestion3.entries.utils.DeleteEntry
+  *       --input=/input/path/to/enriched/
+  *       --output=/output/path/provider/
+  *       --deleteIds=1,2,3,4
+  *       --name=shortName"
+  *       --sparkMaster=local[*]
+  */
+object DeleteEntry extends DeleteExecutor {
+
+  def main(args: Array[String]): Unit = {
+
+    // Read in command line args.
+    val cmdArgs = new CmdArgs(args)
+
+    val dataIn = cmdArgs.getInput
+    val dataOut = cmdArgs.getOutput
+    val shortName = cmdArgs.getProviderName
+    val sparkMaster: Option[String] = cmdArgs.getSparkMaster
+    val deleteIds: String = cmdArgs.getDeleteIds.getOrElse("")
+
+    // If given input path is enrichment, use it as `enrichedData'.
+    // If not, assume that it is a directory containing several enriched sets and
+    // get the most recent enrichment from that directory.
+    val enrichedData = InputHelper.isActivityPath(dataIn) match {
+      case true => dataIn
+      case false => InputHelper.mostRecent(dataIn)
+        .getOrElse(throw new RuntimeException("Unable to load enriched data."))
+    }
+
+    val baseConf =
+      new SparkConf()
+        .setAppName(s"DELETE: $shortName")
+
+    val sparkConf = sparkMaster match {
+      case Some(m) => baseConf.setMaster(m)
+      case None => baseConf
+    }
+
+    val logger = Utils.createLogger("delete")
+
+    executeDelete(sparkConf, enrichedData, dataOut, deleteIds, shortName, logger)
+  }
+}
