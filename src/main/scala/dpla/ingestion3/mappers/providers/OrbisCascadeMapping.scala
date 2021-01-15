1d46e7ced8a41aa1e672ccc589e2e2ec0c95e9fb
Scott Williams <scott@dp.la>
2021-01-15 04:38:14 -0700

    Rewrite orbis mapping. In progess

--- src/main/scala/dpla/ingestion3/mappers/providers/OrbisCascadeMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/OrbisCascadeMapping.scala
@@ -2,11 +2,9 @@ package dpla.ingestion3.mappers.providers
 
 import java.net.URL
 
-import dpla.ingestion3.enrichments.normalizations.StringNormalizationUtils._
-import dpla.ingestion3.enrichments.normalizations.filters.{DigitalSurrogateBlockList, ExtentIdentificationList, FormatTypeValuesBlockList}
 import dpla.ingestion3.mappers.utils.{Document, XmlExtractor, XmlMapping}
 import dpla.ingestion3.messages.IngestMessageTemplates
-import dpla.ingestion3.model.DplaMapData.{ExactlyOne, LiteralOrUri, ZeroToMany, ZeroToOne}
+import dpla.ingestion3.model.DplaMapData.{ExactlyOne, ZeroToMany, ZeroToOne}
 import dpla.ingestion3.model._
 import dpla.ingestion3.utils.Utils
 import org.json4s.JValue
@@ -16,80 +14,77 @@ import scala.util.{Failure, Success, Try}
 import scala.xml._
 
 class OrbisCascadeMapping extends XmlMapping with XmlExtractor with IngestMessageTemplates {
-
-  /**
-    * Fixes
-    * - both freetext rights and edmRights are mapped to the same field
-    * - dc:relation contains both collection title and URLs
-    * - subject -- split on ;
-    * - dc:identifier includes isShownAt value and also local ids
-    *
-    */
-
   // ID minting functions
   override def useProviderName(): Boolean = true
 
   override def getProviderName(): String = "orbis-cascade"
 
-  override def originalId(implicit data: Document[NodeSeq]): ZeroToOne[String] =
-    extractString(data \ "header" \ "identifier")
+  override def originalId(implicit data: Document[NodeSeq]): ZeroToOne[String] ={
+    Option(data
+      .head
+      .attributes
+      .headOption
+      .getOrElse(throw new Exception("Missing required record ID"))
+      .value
+      .text)
+  }
+
 
   // SourceResource mapping
     override def contributor(data: Document[NodeSeq]): Seq[EdmAgent] =
-      extractStrings(metadataRoot(data) \ "contributor")
+      extractStrings(data \\ "SourceResource" \ "contributor")
         .map(nameOnlyAgent)
 
-  // done
+
   override def creator(data: Document[NodeSeq]): Seq[EdmAgent] =
     extractStrings(metadataRoot(data) \ "creator")
       .map(nameOnlyAgent)
 
-  // done
+
   override def date(data: Document[NodeSeq]): Seq[EdmTimeSpan] =
     extractStrings(metadataRoot(data) \ "date")
       .map(stringOnlyTimeSpan)
 
   // done
   override def description(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(metadataRoot(data) \ "description")
+    extractStrings(data \\ "SourceResource" \ "description")
+
+//  override def identifier(data: Document[NodeSeq]): Seq[String] =
+//    extractStrings(metadataRoot(data) \ "identifier")
+//      .filterNot(t => {
+//        Try { new URL(t) } match {
+//          case Success(_) => true
+//          case Failure(_) => false
+//        }
+//      })
 
   // done
-  override def identifier(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(metadataRoot(data) \ "identifier")
-      .filterNot(t => {
-        Try { new URL(t) } match {
-          case Success(_) => true
-          case Failure(_) => false
-        }
-      })
+//  override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
+//    extractStrings(metadataRoot(data) \ "language")
+//      .map(nameOnlyConcept)
 
   // done
-  override def language(data: Document[NodeSeq]): Seq[SkosConcept] =
-    extractStrings(metadataRoot(data) \ "language")
-      .map(nameOnlyConcept)
-
-  // done
-  override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
-    extractStrings(metadataRoot(data) \ "publisher")
-      .map(nameOnlyAgent)
+//  override def publisher(data: Document[NodeSeq]): Seq[EdmAgent] =
+//    extractStrings(metadataRoot(data) \ "publisher")
+//      .map(nameOnlyAgent)
 
   // done
   override def place(data: Document[NodeSeq]): Seq[DplaPlace] =
-    extractStrings(metadataRoot(data) \ "coverage")
+    extractStrings(data \\ "SourceResource" \ "spatial")
       .map(nameOnlyPlace)
 
   // done
   override def subject(data: Document[NodeSeq]): Seq[SkosConcept] =
-    extractStrings(metadataRoot(data) \ "subject")
+    extractStrings(data \\ "SourceResource" \ "subject")
       .map(nameOnlyConcept)
 
   // done
   override def title(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(metadataRoot(data) \ "title")
+    extractStrings(data \\ "SourceResource" \ "title")
 
   // done
   override def `type`(data: Document[NodeSeq]): Seq[String] =
-    extractStrings(metadataRoot(data) \ "type")
+    extractStrings(data \\ "SourceResource" \ "hasType")
 
   // OreAggregation
   override def dplaUri(data: Document[NodeSeq]): ZeroToOne[URI] = mintDplaItemUri(data)
@@ -101,13 +96,22 @@ class OrbisCascadeMapping extends XmlMapping with XmlExtractor with IngestMessag
 
   // done
   override def edmRights(data: Document[NodeSeq]): ZeroToMany[URI] =
-    rightsHelper(data)
+    (data \ "rights").map(r => {
+      r.prefix match {
+        case "edm" => r // FIXME this is bonkers
+          .attributes
+          .headOption
+          .getOrElse(throw new Exception("Missing required record ID"))
+          .value
+          .text
+        case _ => ""
+      }
+    }).filter(_.nonEmpty)
       .filter(URI(_).validate)
       .map(URI)
 
   override def rights(data: Document[NodeSeq]): Seq[String] =
-    rightsHelper(data)
-      .filterNot(URI(_).validate)
+    extractStrings(data \\ "SourceResource" \ "rights")
 
   override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
     extractStrings(metadataRoot(data) \ "identifier")
--- src/test/scala/dpla/ingestion3/mappers/providers/OrbisCascadeMappingTest.scala
+++ src/test/scala/dpla/ingestion3/mappers/providers/OrbisCascadeMappingTest.scala
@@ -20,12 +20,14 @@ class OrbisCascadeMappingTest extends FlatSpec with BeforeAndAfter {
     assert(extractor.useProviderName())
 
   it should "extract the correct original identifier " in
-    assert(extractor.originalId(xml) === Some("oai:digital.lib.uidaho.edu:psychiana/0"))
+    assert(extractor.originalId(xml) === Some("http://harvester.orbiscascade.org/record/e466e93cf4849fd8aa36f1daa1417c15"))
 
-  it should "extract the correct creators" in {
-    val expected = Seq("Robinson, Frank B.").map(nameOnlyAgent)
-    assert(extractor.creator(xml) == expected)
+  it should "extract the correct contributor" in {
+    val expected = Seq("University of Washington Libraries, Special Collections").map(nameOnlyAgent)
+    assert(extractor.contributor(xml) == expected)
   }
+
+  // tbd creator
 
   it should "extract the correct dates" in {
     val expected = Seq("1946").map(stringOnlyTimeSpan)
@@ -33,12 +35,18 @@ class OrbisCascadeMappingTest extends FlatSpec with BeforeAndAfter {
   }
 
   it should "extract the correct description" in
-    assert(extractor.description(xml) == Seq("A description"))
+    assert(extractor.description(xml) == Seq("PH Coll 41.96c Scanned from an original photographic print at 110 dpi in JPEG format at compression rate 3 and resized to 768x600 ppi. 2015"))
 
   it should "extract the correct identifier" in {
     val expected = Seq("MG101_b5_f1_001")
     assert(extractor.identifier(xml) === expected)
   }
+
+  it should "extract the correct place" in {
+    val expected = Seq("Canada--Yukon Territory").map(nameOnlyPlace)
+    assert(extractor.place(xml) === expected)
+  }
+
 
   it should "extract the correct language" in {
     val expected = Seq("eng").map(nameOnlyConcept)
@@ -51,46 +59,27 @@ class OrbisCascadeMappingTest extends FlatSpec with BeforeAndAfter {
   }
 
   it should "extract edmRights value if it is a URI'" in {
-    val rightsXml: Document[NodeSeq] = Document(<record xmlns:dc="http://purl.org/dc/elements/1.1/">
-      <metadata>
-        <oai_dc:dc>
-          <dc:rights>https://rightsstatements.org/vocab/Inc/1.0/</dc:rights>
-        </oai_dc:dc>
-      </metadata>
-    </record>)
-    val expected = Seq(URI("https://rightsstatements.org/vocab/Inc/1.0/"))
-    assert(extractor.edmRights(rightsXml) === expected)
+    val expected = Seq(URI("http://rightsstatements.org/vocab/CNE/1.0/"))
+    assert(extractor.edmRights(xml) === expected)
   }
 
   it should "extract rights value it is text'" in {
-    val rightsXml: Document[NodeSeq] = Document(<record xmlns:dc="http://purl.org/dc/elements/1.1/">
-      <metadata>
-        <oai_dc:dc>
-          <dc:rights>rights statement</dc:rights>
-        </oai_dc:dc>
-      </metadata>
-    </record>)
-    val expected = Seq("rights statement")
-    assert(extractor.rights(rightsXml) === expected)
+    val expected = Seq("For information on permissions for use and reproductions please visit UW Libraries Special Collections Use Permissions page:   http://www.lib.washington.edu/specialcollections/services/permission-for-use")
+    assert(extractor.rights(xml) === expected)
   }
 
   it should "extract the correct subjects" in {
-    val expected = Seq("God-Power; Psychiana Criticism; Psychiana Origins; God on Earth").map(nameOnlyConcept)
+    val expected = Seq("Mountains--Yukon").map(nameOnlyConcept)
     assert(extractor.subject(xml) === expected)
   }
 
   it should "extract the correct titles" in {
-    val expected = Seq("We Read an Amazing Story, Chapter 4 with Editors Corrections 2")
+    val expected = Seq("View of opposite shore from water with mountain in distance, Yukon Territory, circa 1903-1906")
     assert(extractor.title(xml) === expected)
   }
 
   it should "extract the correct types" in {
-    val expected = Seq("Text")
+    val expected = Seq("Photograph")
     assert(extractor.`type`(xml) === expected)
-  }
-
-  it should "extract the correct edmRights" in {
-    val expected = Seq(URI("http://rightsstatements.org/vocab/InC-EDU/1.0/"))
-    assert(extractor.edmRights(xml) === expected)
   }
 }
