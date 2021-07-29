bc055d6bca1dc0c8f62fa97786d325320e6648c8
scott <scott.brandon.williams@gmail.com>
2021-07-29 07:51:37 -0600

    Thumbnail purge
    - Add thumbnail purge executor and entry to remove all cached thumbnails from a specified s3 bucket (#438)
    - Add S3FileHeleper to do deletes

---
+++ src/main/scala/dpla/ingestion3/executors/ThumbnailPurgeExecutor.scala
@@ -0,0 +1,40 @@
+
+
+package dpla.ingestion3.executors
+
+import com.databricks.spark.avro._
+import dpla.ingestion3.utils.S3FileHelper
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+/**
+  * Executes the deletion of thumbnails from the specified bucket
+  *
+  */
+object ThumbnailPurgeExecutor extends S3FileHelper {
+
+  private lazy val dplaUriPrefix = "http://dp.la/api/items/"
+
+  def execute(spark: SparkSession, input: String, bucket: String): Unit = {
+
+    val df = spark.read.avro(input)
+
+    df.createOrReplaceTempView("df")
+
+    val thumbnailKeys = spark.sql("SELECT df.dplaUri from df")
+      .withColumn("dplaId", regexp_replace(col("dplaUri"), dplaUriPrefix, ""))
+      .select("dplaId")
+      .collect()
+      .map(row => thumbnailKey(row.getString(0)))
+
+     deleteS3Keys(bucket, thumbnailKeys)
+  }
+
+  /**
+    * Build key for thumbnail from DPLA ID
+    *
+    * @param id   DPLA ID
+    * @return     S3 object key
+    */
+  def thumbnailKey(id: String): String = s"${id(0)}/${id(1)}/${id(2)}/${id(3)}/$id.jpg"
+}
