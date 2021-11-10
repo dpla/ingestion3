4fffc0c8f9fad79876fee20e0d743caca35115c5
Scott Williams <scott@dp.la>
2021-11-10 10:46:41 -0700

    NYPL alchemy

--- src/main/scala/dpla/ingestion3/mappers/providers/NYPLMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/NYPLMapping.scala
@@ -1,17 +1,49 @@
 package dpla.ingestion3.mappers.providers
 
 import dpla.ingestion3.enrichments.normalizations.filters.{DigitalSurrogateBlockList, FormatTypeValuesBlockList}
-import dpla.ingestion3.mappers.utils.{Document, XmlExtractor, XmlMapping}
+import dpla.ingestion3.mappers.utils
+import dpla.ingestion3.mappers.utils._
 import dpla.ingestion3.messages.IngestMessageTemplates
 import dpla.ingestion3.model.DplaMapData._
-import dpla.ingestion3.model._
+import dpla.ingestion3.model.{EdmWebResource, _}
 import dpla.ingestion3.utils.Utils
+import org.json4s
 import org.json4s.JValue
 import org.json4s.JsonDSL._
 
-import scala.xml._
+import scala.xml.{Elem, NodeSeq, XML}
 
-class NYPLMapping extends XmlMapping with XmlExtractor with IngestMessageTemplates {
+class NyplJsonExtractor extends JsonExtractor
+class NyplXmlExtractor extends XmlExtractor
+
+class NyplMapping(doc: Document[JValue]) extends JsonMapping with IngestMessageTemplates {
+
+  // extractors
+  lazy val json: NyplJsonExtractor = new NyplJsonExtractor
+  lazy val xml: NyplXmlExtractor = new NyplXmlExtractor
+
+  // mods xml from json
+  lazy val modsXml: Elem = XML.loadString(json.extractString(modsRoot(doc)).getOrElse(""))
+
+  private def modsRoot(data: JValue): JValue = data \ "solr_doc_hash" \ "mods_st"
+  private def solrRoot(data: JValue): JValue = data \ "solr_doc_hash"
+
+  /**
+    * Does the provider use a prefix (typically their provider shortname/abbreviation) to
+    * salt the base identifier?
+    *
+    * @return Boolean
+    */
+  override def useProviderName: Boolean = true
+
+  /**
+    * Extract the record's "persistent" identifier. Implementations should raise
+    * an Exception if no ID can be extracted
+    *
+    * @return String Record identifier
+    * @throws Exception If ID can not be extracted
+    */
+  override def originalId(implicit data: utils.Document[JValue]): ZeroToOne[String] = json.extractString("uuid")(data)
 
   val formatBlockList: Set[String] =
     DigitalSurrogateBlockList.termList ++
@@ -37,7 +69,8 @@ class NYPLMapping extends XmlMapping with XmlExtractor with IngestMessageTemplat
     "curator","curator of an exhibition","dancer","data contributor","data manager","dedicatee","defendant","defendant-appellant",
     "defendant-appellee","degree granting institution","degree grantor","delineator","depositor","distributor","donor",
     "draftsman","editor","editor of compilation","editor of moving image work","electrician","electrotyper","engineer","expert",
-    "field director","film distributor","film editor","film producer","first party","former owner","funder","geographic information specialist","honoree","host","illuminator","inscriber","instrumentalist","issuing body","judge","laboratory",
+    "field director","film distributor","film editor","film producer","first party","former owner","funder",
+    "geographic information specialist","honoree","host","illuminator","inscriber","instrumentalist","issuing body","judge","laboratory",
     "laboratory director","lead","lender","libelant","libelant-appellant","libelant-appellee","libelee","libelee-appellant",
     "libelee-appellee","licensee","licensor","manufacturer","marbler","markup editor","metadata contact","metal-engraver",
     "moderator","monitor","music copyist","musical director","musician","narrator","opponent","organizer","originator",
@@ -52,124 +85,114 @@ class NYPLMapping extends XmlMapping with XmlExtractor with IngestMessageTemplat
     "thesis advisor","transcriber","translator","type designer","typographer","videographer","voice actor","witness",
     "writer of accompanying material")
 
-  // ID minting functions
-  override def useProviderName(): Boolean = true
+  override def title(data: Document[json4s.JValue]): AtLeastOne[String] =
+  // titleInfo \ "title" @usage='primary'
+    (modsXml \ "titleInfo")
+      .flatMap(node => xml.getByAttribute(node, "usage", "primary"))
+      .flatMap(node => xml.extractStrings (node \ "title"))
 
-  override def getProviderName(): String = "nypl"
-
-  override def originalId(implicit data: Document[NodeSeq]): ZeroToOne[String] =
-    extractString(data \ "identifier")
-      .map(_.trim)
-
-  override def title(data: Document[NodeSeq]): Seq[String] =
-      // titleInfo \ "title" @usage='primary'
-      (data \ "titleInfo")
-        .flatMap(node => getByAttribute(node, "usage", "primary"))
-        .flatMap(node => extractStrings (node \ "title"))
-
-  override def alternateTitle(data: Document[NodeSeq]): ZeroToMany[String] =
-    // all other title values
-    extractStrings(data \ "titleInfo" \ "title")
+  override def alternateTitle(data: Document[json4s.JValue]): ZeroToMany[String] =
+  // all other title values
+    xml.extractStrings(modsXml \ "titleInfo" \ "title")
       .diff(title(data))
 
-  override def identifier(data: Document[NodeSeq]): ZeroToMany[String] = {
+  override def identifier(data: Document[json4s.JValue]): ZeroToMany[String] = {
     val types = Seq("local_imageid", "isbn", "isrc", "isan", "ismn", "iswc", "issn", "uri", "urn")
 
-    (data \ "identifier")
-      .filter(node => filterAttributeListOptions(node, "type", types) || filterAttributeListOptions(node, "displayLabel", types))
-      .flatMap(extractStrings)
+    (modsXml \ "identifier")
+      .filter(node => xml.filterAttributeListOptions(node, "type", types) || xml.filterAttributeListOptions(node, "displayLabel", types))
+      .flatMap(xml.extractStrings)
   }
 
-  override def description(data: Document[NodeSeq]): ZeroToMany[String] = {
+  override def description(data: Document[json4s.JValue]): ZeroToMany[String] = {
     // note @type='content'
     // abstract
-    (data \ "note")
-      .flatMap(node => getByAttribute(node, "type", "content"))
-      .flatMap(extractStrings) ++
-    extractStrings(data \ "abstract")
+    (modsXml \ "note")
+      .flatMap(node => xml.getByAttribute(node, "type", "content"))
+      .flatMap(xml.extractStrings) ++ xml.extractStrings(modsXml \ "abstract")
   }
 
-  override def isShownAt(data: Document[NodeSeq]): ZeroToMany[EdmWebResource] =
+  override def isShownAt(data: Document[json4s.JValue]): ZeroToMany[EdmWebResource] =
   // For the base URL was https://digitalcollections.nypl.org/items/
   // plus
   // uuid 4d0e0bc0-c540-012f-1857-58d385a7bc34
   // twas ever thus
   // https://digitalcollections.nypl.org/items/4d0e0bc0-c540-012f-1857-58d385a7bc34
-    (data \ "identifier")
-      .flatMap(node => getByAttribute(node, "type", "uuid"))
-      .flatMap(extractStrings)
+    (modsXml \ "identifier")
+      .flatMap(node => xml.getByAttribute(node, "type", "uuid"))
+      .flatMap(xml.extractStrings)
       .map(uuid => s"https://digitalcollections.nypl.org/items/$uuid")
       .map(stringOnlyWebResource)
 
-  override def subject(data: Document[NodeSeq]): ZeroToMany[SkosConcept] = {
+  override def subject(data: Document[json4s.JValue]): ZeroToMany[SkosConcept] = {
     val subjectKeys = Seq("topic", "geographic", "temporal", "occupation")
 
-    val subjectTitles = extractStrings(data \ "subject" \ "titleInfo" \ "title")
-    val subjectNames = extractStrings(data \ "subject" \ "name" \ "namePart")
+    val subjectTitles = xml.extractStrings(modsXml \ "subject" \ "titleInfo" \ "title")
+    val subjectNames = xml.extractStrings(modsXml \ "subject" \ "name" \ "namePart")
     val subjects = subjectKeys
-      .map(key => data \ "subject" \ key)
-      .flatMap(extractStrings)
+      .map(key => modsXml \ "subject" \ key)
+      .flatMap(xml.extractStrings)
 
     (subjects ++ subjectNames ++ subjectTitles).map(nameOnlyConcept)
   }
 
-  override def `type`(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "typeOfResource")
+  override def `type`(data: Document[json4s.JValue]): ZeroToMany[String] =
+    xml.extractStrings(modsXml \ "typeOfResource")
 
-  override def format(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "physicalDescription" \ "format") ++
-    extractStrings(data \ "genre")
+  override def format(data: Document[json4s.JValue]): ZeroToMany[String] =
+    xml.extractStrings(modsXml \ "physicalDescription" \ "format") ++
+      xml.extractStrings(modsXml \ "genre")
 
-  override def extent(data: Document[NodeSeq]): ZeroToMany[String] =
-    extractStrings(data \ "physicalDescription" \ "extent")
+  override def extent(data: Document[json4s.JValue]): ZeroToMany[String] =
+    xml.extractStrings(modsXml \ "physicalDescription" \ "extent")
 
-  override def temporal(data: Document[NodeSeq]): ZeroToMany[EdmTimeSpan] =
-    extractStrings(data \ "subject" \ "temporal").map(stringOnlyTimeSpan)
+  override def temporal(data: Document[json4s.JValue]): ZeroToMany[EdmTimeSpan] =
+    xml.extractStrings(modsXml \ "subject" \ "temporal").map(stringOnlyTimeSpan)
 
-  override def creator(data: Document[NodeSeq]): ZeroToMany[EdmAgent] = agentHelper(data, creatorRoles)
+  override def creator(data: Document[json4s.JValue]): ZeroToMany[EdmAgent] = agentHelper(modsXml, creatorRoles)
 
-  override def contributor(data: Document[NodeSeq]): ZeroToMany[EdmAgent] = agentHelper(data, contributorRoles)
+  override def contributor(data: Document[json4s.JValue]): ZeroToMany[EdmAgent] = agentHelper(modsXml, contributorRoles)
 
-  override def collection(data: Document[NodeSeq]): ZeroToMany[DcmiTypeCollection] = ???
-    // This mapping seems overly complicated
+  override def collection(data: Document[json4s.JValue]): ZeroToMany[DcmiTypeCollection] = ???
+  // This mapping seems overly complicated
 
-  override def date(data: Document[NodeSeq]): ZeroToMany[EdmTimeSpan] = ???
-    // this is complicated
-
-  override def publisher(data: Document[NodeSeq]): ZeroToMany[EdmAgent] = ???
+  override def date(data: Document[json4s.JValue]): ZeroToMany[EdmTimeSpan] = ???
   // this is complicated
 
-  override def edmRights(data: Document[NodeSeq]): ZeroToMany[URI] = ???
-    // rights information appears to exist outside the metadata record an inside the JSON block
+  override def publisher(data: Document[json4s.JValue]): ZeroToMany[EdmAgent] = ???
+  // this is complicated
 
-  override def rights(data: Document[NodeSeq]): AtLeastOne[String] = ???
-    // same problem as edmRights
+  override def edmRights(data: Document[json4s.JValue]): ZeroToMany[URI] = ???
+  // rights information appears to exist outside the metadata record an inside the JSON block
 
-  override def place(data: Document[NodeSeq]): ZeroToMany[DplaPlace] =
-    extractStrings(data \ "subject" \ "geographic").map(nameOnlyPlace)
+  override def rights(data: Document[json4s.JValue]): AtLeastOne[String] = ???
+  // same problem as edmRights
 
-  override def language(data: Document[NodeSeq]): ZeroToMany[SkosConcept] =
-    extractStrings(data \ "language" \ "languageTerm").map(nameOnlyConcept)
+  override def place(data: Document[json4s.JValue]): ZeroToMany[DplaPlace] =
+    xml.extractStrings(modsXml \ "subject" \ "geographic").map(nameOnlyPlace)
+
+  override def language(data: Document[json4s.JValue]): ZeroToMany[SkosConcept] =
+    xml.extractStrings(modsXml \ "language" \ "languageTerm").map(nameOnlyConcept)
 
   // OreAggregation
-  override def dplaUri(data: Document[NodeSeq]): ZeroToOne[URI] = mintDplaItemUri(data)
+  override def dplaUri(data: Document[json4s.JValue]): ZeroToOne[URI] = mintDplaItemUri(data)
 
-  override def originalRecord(data: Document[NodeSeq]): ExactlyOne[String] = Utils.formatXml(data)
+  override def originalRecord(data: Document[json4s.JValue]): ExactlyOne[String] = Utils.formatJson(data)
 
-  override def provider(data: Document[NodeSeq]): ExactlyOne[EdmAgent] = agent
+  override def provider(data: Document[json4s.JValue]): ExactlyOne[EdmAgent] = agent
 
-  override def dataProvider(data: Document[NodeSeq]): ZeroToMany[EdmAgent] = {
-    (data \ "location" \ "physicalLocation")
-      .filterNot(node => filterAttribute(node, "authority", "marcorg")) // FIXME, this filter isn't working as expected
-      .map(node => getByAttribute(node, "type", "division"))
-      .flatMap(extractStrings)
+  override def dataProvider(data: Document[json4s.JValue]): ZeroToMany[EdmAgent] = {
+    (modsXml \ "location" \ "physicalLocation")
+      .filterNot(node => xml.filterAttribute(node, "authority", "marcorg")) // FIXME, this filter isn't working as expected
+      .map(node => xml.getByAttribute(node, "type", "division"))
+      .flatMap(xml.extractStrings)
       .map(_.stripSuffix(".").trim)
       .map(dataProvider => s"$dataProvider. The New York Public Library")
       .distinct
       .map(nameOnlyAgent)
   }
 
-  override def sidecar(data: Document[NodeSeq]): JValue =
+  override def sidecar(data: Document[json4s.JValue]): JValue =
     ("prehashId" -> buildProviderBaseId()(data)) ~ ("dplaId" -> mintDplaId(data))
 
   // Helper method
@@ -178,12 +201,14 @@ class NYPLMapping extends XmlMapping with XmlExtractor with IngestMessageTemplat
     uri = Some(URI("http://dp.la/api/contributor/nypl"))
   )
 
-  def agentHelper(data: Document[NodeSeq], roles: Seq[String]): ZeroToMany[EdmAgent] =
+  def agentHelper(data: NodeSeq, roles: Seq[String]): ZeroToMany[EdmAgent] =
     (data \ "name")
       .filter(node =>
-        extractStrings(node \ "role" \ "roleTerm")
+        xml.extractStrings(node \ "role" \ "roleTerm")
           .map(_.toLowerCase.stripSuffix(" ."))
           .exists(roles.contains(_)))
-      .flatMap(node => extractStrings(node \ "namePart"))
+      .flatMap(node => xml.extractStrings(node \ "namePart"))
       .map(nameOnlyAgent)
 }
+
+
