b0214ac5ba2a31f6d1b70939c51bceb31a94aeb0
Michael Della Bitta <mdellabitta@tutanota.com>
2022-07-19 11:10:06 -0400

    wip

--- src/main/scala/dpla/ingestion3/mappers/providers/JsonRetconMapper.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/JsonRetconMapper.scala
@@ -3,121 +3,141 @@ package dpla.ingestion3.mappers.providers
 import dpla.ingestion3.enrichments.normalizations.filters.{DigitalSurrogateBlockList, ExtentIdentificationList, FormatTypeValuesBlockList}
 import dpla.ingestion3.mappers.utils.{Document, JsonExtractor, JsonMapping}
 import dpla.ingestion3.model.DplaMapData.{AtLeastOne, ExactlyOne, ZeroToMany, ZeroToOne}
-import dpla.ingestion3.model.{DcmiTypeCollection, DplaPlace, EdmAgent, EdmTimeSpan, EdmWebResource, LiteralOrUri, SkosConcept, URI, eitherStringOrUri, nameOnlyAgent, nameOnlyCollection, nameOnlyConcept, nameOnlyPlace, stringOnlyTimeSpan}
+import dpla.ingestion3.model.{DcmiTypeCollection, DcmiTypes, DplaPlace, EdmAgent, EdmTimeSpan, EdmWebResource, LiteralOrUri, SkosConcept, URI, eitherStringOrUri, emptyEdmAgent, emptyJValue, emptySeq, emptyString, nameOnlyAgent, nameOnlyCollection, nameOnlyConcept, nameOnlyPlace, stringOnlyTimeSpan, uriOnlyWebResource}
 import dpla.ingestion3.utils.Utils
 import org.json4s
 import org.json4s.JValue
+import org.json4s.JsonAST.{JArray, JObject, JString, JValue}
 
-class JsonRetconMapper extends JsonMapping with JsonExtractor {
-  
-  // ID minting functions
-  override def useProviderName: Boolean = true
+abstract class JsonRetconMapper extends JsonMapping with JsonExtractor {
 
-  // Hard coded to prevent accidental changes to base ID
-  override def getProviderName: Option[String] = Some("cdl")
+  // OreAggregation
+  override def dplaUri(data: Document[JValue]): ZeroToOne[URI] = {
+    data.get \ "_source" \ "@id" match {
+      case JString(value) => Some(URI(f"http://dp.la/api/items/$value"))
+      case _ => None
+    }
+  }
 
-  // OreAggregation fields
-  override def dplaUri(data: Document[JValue]): ZeroToOne[URI] = mintDplaItemUri(data)
+  override def dataProvider(data: Document[JValue]): ZeroToMany[EdmAgent] =
+    extractStrings(data.get \ "_source" \ "dataProvider")
+      .map(nameOnlyAgent)
 
-  override def originalId(implicit data: Document[JValue]): ZeroToOne[String] = extractString("id")(data)
+  override def originalRecord(data: Document[JValue]): ExactlyOne[String] =
+    extractStrings(data.get \ "_source" \ "originalRecord")
+      .headOption
+      .getOrElse("")
 
-  override def sidecar(data: Document[JValue]): JValue =
-    ("prehashId", buildProviderBaseId()(data)) ~ ("dplaId", mintDplaId(data))
+  override def isShownAt(data: Document[JValue]): ZeroToMany[EdmWebResource] =
+    extractStrings(data.get \ "_source" \ "isShownAt")
+      .map(URI)
+      .map(uriOnlyWebResource)
 
-  override def dataProvider(data: Document[JValue]): ZeroToMany[EdmAgent] = Seq(nameOnlyAgent(getDataProvider(data)))
+
+  //todo: object vs preview
+  override def `object`(data: Document[JValue]): ZeroToMany[EdmWebResource] =
+    extractStrings(data.get \ "_source" \ "object")
+      .map(URI)
+      .map(uriOnlyWebResource)
+
+  override def provider(data: Document[JValue]): ExactlyOne[EdmAgent] =
+    extractStrings(data.get \ "_source" \ "provider" \ "name")
+      .map(nameOnlyAgent)
+      .headOption
+      .getOrElse(EdmAgent())
 
   override def edmRights(data: Document[JValue]): ZeroToMany[URI] =
-    extractStrings("rights_uri")(data)
+    extractStrings(data.get \ "_source" \ "rights")
       .map(URI)
 
-  override def originalRecord(data: Document[JValue]): ExactlyOne[String] = Utils.formatJson(data)
 
-  override def preview(data: Document[JValue]): ZeroToMany[EdmWebResource] = thumbnail(data)
-
-  override def provider(data: Document[JValue]): ExactlyOne[EdmAgent] = EdmAgent(
-    name = Some("California Digital Library"),
-    uri = Some(URI("http://dp.la/api/contributor/cdl"))
-  )
-
-  override def isShownAt(data: Document[JValue]): ZeroToMany[EdmWebResource] = providerUri(data)
+  // this function deals with the fact that ingestion1 was a pile
+  // that let you emit anything
+  private def maybeArray[T](
+                             value: JValue,
+                             f: List[(String, JValue)] => Option[T]
+                           ): Seq[T] = value match {
+    case JObject(fields) =>
+      f(fields).toSeq
+    case JArray(values) =>
+      values.flatMap({case JObject(fields) => f(fields)})
+    case _ =>
+      Seq()
+  }
 
   // SourceResource
-  override def alternateTitle(data: Document[JValue]): ZeroToMany[String] =
-    extractStrings("alternative_title_ss")(data)
-
   override def collection(data: Document[JValue]): ZeroToMany[DcmiTypeCollection] =
-    extractStrings("collection_name")(data).map(nameOnlyCollection)
+    maybeArray(
+      data.get \ "_source" \ "sourceResource" \ "collection",
+      (fields) => {
+        val title = fields.find(_._1 == "title").map(_._2.toString)
+        val description = fields.find(_._1 == "description").map(_._2.toString)
+        if (title.nonEmpty) Some(DcmiTypeCollection(title, description))
+        else None
+      }
+    )
 
   override def contributor(data: Document[JValue]): ZeroToMany[EdmAgent] =
-    extractStrings("contributor_ss")(data).map(nameOnlyAgent)
+    extractStrings(data.get \ "_source" \ "sourceResource" \ "contributor")
+      .map(nameOnlyAgent)
 
   override def creator(data: Document[JValue]): ZeroToMany[EdmAgent] =
-    extractStrings("creator_ss")(data).map(nameOnlyAgent)
+    extractStrings(data.get \ "_source" \ "sourceResource" \ "creator")
+      .map(nameOnlyAgent)
 
+  //todo will not work for artstor
   override def date(data: Document[JValue]): ZeroToMany[EdmTimeSpan] =
-    extractStrings("date_ss")(data).map(stringOnlyTimeSpan)
+    maybeArray(
+      data.get \ "_source" \ "sourceResource" \ "date",
+      fields => {
+        val begin = fields.find(_._1 == "begin").map(_._2.toString)
+        val end = fields.find(_._1 == "end").map(_._2.toString)
+        val displayDate = fields.find(_._1 == "displayDate").map(_._2.toString)
+
+        if (begin.nonEmpty && end.nonEmpty && displayDate.nonEmpty) None
+        else Some(
+          EdmTimeSpan(
+            originalSourceDate = displayDate,
+            prefLabel = displayDate,
+            begin = begin,
+            end = end
+          )
+        )
+      }
+    )
 
   override def description(data: Document[JValue]): ZeroToMany[String] =
-    extractStrings("description")(data)
+    maybeArray(
+      data.get \ "_source" \ "sourceResource" \ "description",
+      x => Option(x.toString)
+    )
 
   override def extent(data: Document[JValue]): ZeroToMany[String] =
-    extractStrings("extent_ss")(data) ++
-      extentFromFormat(data)
+    extractStrings(data.get \ "_source" \ "sourceResource" \ "extent")
 
   override def format(data: Document[JValue]): ZeroToMany[String] =
-    extractStrings("format")(data)
-      .map(_.applyBlockFilter(
-        DigitalSurrogateBlockList.termList ++
-          FormatTypeValuesBlockList.termList ++
-          ExtentIdentificationList.termList))
-      .filter(_.nonEmpty)
+    extractStrings(data.get \ "_source" \ "sourceResource" \ "format") ++
+      extractStrings(data.get \ "_source" \ "sourceResource" \ "hasFormat")
 
   override def genre(data: Document[JValue]): ZeroToMany[SkosConcept] =
-    extractStrings("genre_ss")(data).map(nameOnlyConcept)
-
-  override def identifier(data: Document[JValue]): ZeroToMany[String] =
-    extractStrings("identifier_ss")(data)
-
-  override def language(data: Document[JValue]): ZeroToMany[SkosConcept] =
-    extractStrings("language_ss")(data).map(nameOnlyConcept)
-
-  override def place(data: Document[JValue]): ZeroToMany[DplaPlace] =
-    extractStrings("coverage_ss")(data).map(nameOnlyPlace)
-
-  override def publisher(data: Document[JValue]): ZeroToMany[EdmAgent] =
-    extractStrings("publisher_ss")(data).map(nameOnlyAgent)
-
-  override def relation(data: Document[JValue]): ZeroToMany[LiteralOrUri] =
-    extractStrings("relation_ss")(data).map(eitherStringOrUri)
-
-  override def rights(data: Document[JValue]): AtLeastOne[String] =
-    extractStrings("rights_ss")(data) ++
-      extractStrings("rights_note_ss")(data) ++
-      extractStrings("rights_date_ss")(data) ++
-      extractStrings("rightsholder_ss")(data)
-
-  override def subject(data: Document[JValue]): ZeroToMany[SkosConcept] =
-    extractStrings("subject_ss")(data)
-      .map(_.cleanupLeadingPunctuation)
-      .map(_.cleanupEndingPunctuation)
-      .map(_.stripEndingPeriod)
-      .flatMap(_.splitAtDelimiter(";"))
+    extractStrings(data.get \ "_source" \ "sourceResource" \ "genre")
       .map(nameOnlyConcept)
 
-  override def temporal(data: Document[JValue]): ZeroToMany[EdmTimeSpan] =
-    extractStrings("temporal_ss")(data).map(stringOnlyTimeSpan)
+  override def identifier(data: Document[JValue]): ZeroToMany[String] =
+    extractStrings(data.get \ "_source" \ "sourceResource" \ "identifier")
 
-  override def title(data: Document[JValue]): AtLeastOne[String] =
-    extractStrings("title_ss")(data).map(_.stripBrackets)
 
-  override def `type`(data: Document[JValue]): ZeroToMany[String] = extractStrings("type")(data)
-
-  override def intermediateProvider(data: Document[json4s.JValue]): ZeroToOne[EdmAgent] = {
-    val repositories = extractStrings("repository_name")(data)
-    if(repositories.tail.nonEmpty)
-      repositories.take(2).lastOption.map(nameOnlyAgent)
-    else
-      None
-  }
+  override def language(data: Document[JValue]): ZeroToMany[SkosConcept] = ???
+  override def place(data: Document[JValue]): ZeroToMany[DplaPlace] = ???
+  override def publisher(data: Document[JValue]): ZeroToMany[EdmAgent] = ???
+  override def relation(data: Document[JValue]): ZeroToMany[LiteralOrUri] = ???
+  override def replacedBy(data: Document[JValue]): ZeroToMany[String] = ???
+  override def replaces(data: Document[JValue]): ZeroToMany[String] = ???
+  override def rights(data: Document[JValue]): AtLeastOne[String] = ???
+  override def rightsHolder(data: Document[JValue]): ZeroToMany[EdmAgent] = ???
+  override def subject(data: Document[JValue]): ZeroToMany[SkosConcept] = ???
+  override def temporal(data: Document[JValue]): ZeroToMany[EdmTimeSpan] = ???
+  override def title(data: Document[JValue]): AtLeastOne[String] = ???
+  override def `type`(data: Document[JValue]): ZeroToMany[String] = ???
 
 }
