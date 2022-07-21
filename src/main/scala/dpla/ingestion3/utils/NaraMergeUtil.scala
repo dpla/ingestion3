9e1c094c05f55f551d0899f1fea95880cfb597b8
scott <scott.brandon.williams@gmail.com>
2022-07-21 06:52:34 -0600

    Been writing the wrong dataset to disk for the NARA harvest (without the deleted records)

--- src/main/scala/dpla/ingestion3/utils/NaraMergeUtil.scala
+++ src/main/scala/dpla/ingestion3/utils/NaraMergeUtil.scala
@@ -197,9 +197,9 @@ object NaraMergeUtil {
 
     val summary = getLogs(logs)
 
-    // Write merged data
+    // Write merged data with deletes
     new File(outputPath).mkdirs()
-    mergedDf
+    mergedWithDeletesDf
       .write
       .mode(SaveMode.Overwrite)
       .avro(outputPath)
