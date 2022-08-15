cbcc1d7ab1309061f85482a7a58510e12517e11c
scott <scott.brandon.williams@gmail.com>
2022-08-15 13:17:37 -0600

    Remove unused mappings

--- src/main/scala/dpla/ingestion3/mappers/providers/CommunityWebsMapping.scala
+++ src/main/scala/dpla/ingestion3/mappers/providers/CommunityWebsMapping.scala
@@ -1,7 +1,5 @@
 package dpla.ingestion3.mappers.providers
 
-import dpla.ingestion3.enrichments.TaggingUtils._
-import dpla.ingestion3.enrichments.normalizations.filters.ExtentIdentificationList
 import dpla.ingestion3.mappers.utils._
 import dpla.ingestion3.messages.IngestMessageTemplates
 import dpla.ingestion3.model.DplaMapData.{AtLeastOne, ExactlyOne, ZeroToMany, ZeroToOne}
@@ -29,7 +27,7 @@ class CommunityWebsMapping extends JsonMapping with JsonExtractor with IngestMes
 
   override def edmRights(data: Document[json4s.JValue]): ZeroToMany[URI] =
     extractStrings(unwrap(data) \ "rights_statement")
-      .filter(_.nonEmpty) // FIXME filtering non-empty values should be a standard edmRights normalization
+      .filter(_.nonEmpty)
       .map(URI)
 
   override def isShownAt(data: Document[JValue]): ZeroToMany[EdmWebResource] =
@@ -47,11 +45,6 @@ class CommunityWebsMapping extends JsonMapping with JsonExtractor with IngestMes
   override def sidecar(data: Document[JValue]): JValue =
     ("prehashId", buildProviderBaseId()(data)) ~ ("dplaId", mintDplaId(data))
 
-  // SourceResource
-//  override def contributor(data: Document[JValue]): ZeroToMany[EdmAgent] =
-//    extractStrings(unwrap(data) \ "sourceResource" \ "contributor" \ "name")
-//      .map(nameOnlyAgent)
-
   override def creator(data: Document[JValue]): ZeroToMany[EdmAgent] =
     extractStrings(unwrap(data) \ "creator")
       .map(nameOnlyAgent)
@@ -67,15 +60,8 @@ class CommunityWebsMapping extends JsonMapping with JsonExtractor with IngestMes
   override def description(data: Document[JValue]): ZeroToMany[String] =
     extractStrings(unwrap(data) \ "description")
 
-//  override def extent(data: Document[JValue]): ZeroToMany[String] =
-//    extractStrings(unwrap(data) \ "sourceResource" \ "extent")
-
   override def format(data: Document[JValue]): ZeroToMany[String] =
     extractStrings(unwrap(data) \ "type")
-
-//  override def genre(data: Document[JValue]): ZeroToMany[SkosConcept] =
-//    extractStrings(unwrap(data) \ "sourceResource" \ "genre" \ "name")
-//      .map(nameOnlyConcept)
 
   override def identifier(data: Document[JValue]): ZeroToMany[String] =
     extractStrings(unwrap(data) \ "sourceResource" \ "identifier")
@@ -88,10 +74,6 @@ class CommunityWebsMapping extends JsonMapping with JsonExtractor with IngestMes
   override def place(data: Document[JValue]): ZeroToMany[DplaPlace] =
     extractStrings(unwrap(data) \ "coverage")
       .map(nameOnlyPlace)
-
-//  override def publisher(data: Document[JValue]): ZeroToMany[EdmAgent] =
-//    extractStrings(unwrap(data) \ "sourceResource" \ "publisher")
-//      .map(nameOnlyAgent)
 
   override def rights(data: Document[JValue]): AtLeastOne[String] =
     extractStrings(unwrap(data) \ "rights_free_text")
