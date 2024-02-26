1e10b7748fca7dfd66b7d78a830b747fa897a7cf
scott <scott.brandon.williams@gmail.com>
2024-02-26 08:31:42 -0700

    Fix bug in SI reporcessing script

--- scripts/fix-si.sh
+++ scripts/fix-si.sh
@@ -1,17 +1,19 @@
 # Fix the Smithsonian data 
 # 
 data_path=${DPLA_DATA}/smithsonian/originalRecords/
-ls $data_path
+ls "$data_path"
 echo ''
 
+# Get date from user input
 read -p "Which folder: " date
 echo "Reading from...$data_path$date"
 
-mkdir $data_path/$date/fixed 
-mkdir $data_path/$date/xmll
+mkdir -p "$data_path"/"$date"/fixed
+mkdir -p "$data_path"/"$date"/xmll
 
 # unzip and rezip
 find $data_path$date/ -name "*.gz" -type f | \
+  # shellcheck disable=SC2016
   xargs -I{} sh -c 'echo "$1" "./$(basename ${1%.*}).${1##*.}"' -- {} | \
   xargs -n 2 -P 8 sh -c 'gunzip -dckv $0 | gzip -kv > '$data_path$date'/fixed/$1'
 
