a7e0a0943bcb23e7c09df01b2b34d5b2492b8f1e
Michael Della Bitta <mdellabitta@tutanota.com>
2024-03-26 12:16:15 -0400

    removed todo

--- src/main/scala/dpla/ingestion3/harvesters/oai/refactor/OaiProtocol.scala
+++ src/main/scala/dpla/ingestion3/harvesters/oai/refactor/OaiProtocol.scala
@@ -7,14 +7,12 @@ class OaiProtocol(oaiConfiguration: OaiConfiguration)
   lazy val endpoint: String = oaiConfiguration.endpoint
   lazy val metadataPrefix: Option[String] = oaiConfiguration.metadataPrefix
 
-  override def listAllRecordPages(): IterableOnce[Either[OaiError, OaiPage]] = {
-    val responseBuilder =
-      new OaiMultiPageResponseBuilder(endpoint, "ListRecords", metadataPrefix)
-
-    val multiPageResponse = responseBuilder.getResponse
-    // TODO: Is there a better way to make a TraversableOnce return type?
-    multiPageResponse.iterator
-  }
+  override def listAllRecordPages(): IterableOnce[Either[OaiError, OaiPage]] =
+    new OaiMultiPageResponseBuilder(
+      endpoint,
+      "ListRecords",
+      metadataPrefix
+    ).getResponse.iterator
 
   override def listAllRecordPagesForSet(
       setEither: Either[OaiError, OaiSet]
@@ -23,7 +21,6 @@ class OaiProtocol(oaiConfiguration: OaiConfiguration)
     val listResponse = setEither match {
       case Left(error) => List(Left(error))
       case Right(set) =>
-
         val responseBuilder = new OaiMultiPageResponseBuilder(
           endpoint,
           "ListRecords",
