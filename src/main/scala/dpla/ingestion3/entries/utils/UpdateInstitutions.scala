ba1da555fb96b4a46128262e76342b21c746406f
Michael Della Bitta <mdellabitta@tutanota.com>
2025-02-28 02:01:55 +0000

    Update institutions file + creator (#630)

--- src/main/scala/dpla/ingestion3/entries/utils/UpdateInstitutions.scala
+++ src/main/scala/dpla/ingestion3/entries/utils/UpdateInstitutions.scala
@@ -1,8 +1,10 @@
 package dpla.ingestion3.entries.utils
 
+import dpla.ingestion3.utils.HttpUtils
 import org.json4s.JsonAST.{JObject, JString}
 import org.json4s.jackson.JsonMethods._
 import org.json4s.jackson.Serialization
+import org.json4s.JsonDSL._
 import org.json4s._
 import org.json4s.prefs.EmptyValueStrategy
 
@@ -10,27 +12,51 @@ import java.io.File
 import java.net.URI
 import java.net.http.HttpClient.Redirect
 import java.net.http.HttpRequest.BodyPublishers
-import java.net.http.HttpResponse.BodyHandlers
+
 import java.net.http.{HttpClient, HttpRequest}
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths}
 import java.time.Duration
+import scala.language.implicitConversions
 
 object UpdateInstitutions {
 
-  private val ELASTICSEARCH_ENDPOINT =
-    "http://search.internal.dp.la:9200/dpla_alias/_search"
-  private val MAX_FACET_BUCKETS = 5000
+  implicit val formats: Formats = DefaultFormats.withEmptyValueStrategy(
+    EmptyValueStrategy.preserve
+  )
+
+  private type HubTuple = (String, Hub)
+  private type ContributingInstitutionTuple = (String, ContributingInstitution)
+
+  implicit def contributingInstitutionToJValue(contributingInstitution: ContributingInstitution): JValue = {
+    ("Wikidata" -> contributingInstitution.Wikidata) ~
+      ("upload" -> contributingInstitution.upload)
+  }
+
+  implicit def hubToJValue(hub: Hub): JValue = {
+    ("Wikidata" -> hub.Wikidata) ~
+      ("institutions" -> hub.institutions) ~
+      ("upload" -> hub.upload)
+  }
+
+  implicit def map2jvalue[T](m: Map[String, T])(implicit ev: T => JValue): JObject =
+    JObject(m.toList.sortWith( (a, b) => {a._1.compareTo(b._1) < 0}).map { case (k, v) => JField(k, ev(v)) })
 
   case class Hub(
-      Wikidata: Option[String],
-      institutions: Map[String, ContributingInstitution]
-  )
+                  Wikidata: Option[String] = Some(""),
+                  institutions: Map[String, ContributingInstitution] = Map(),
+                  upload: Option[Boolean] = Some(false)
+                )
+
   case class ContributingInstitution(
-      Wikidata: Option[String],
-      upload: Option[Boolean]
+      Wikidata: Option[String] = Some(""),
+      upload: Option[Boolean] = Some(false)
   )
-  private def getFormats = DefaultFormats.withEmptyValueStrategy(EmptyValueStrategy.preserve)
+
+  private val ELASTICSEARCH_ENDPOINT =
+    "http://search.internal.dp.la:9200/dpla_alias/_search"
+
+  private val MAX_FACET_BUCKETS = 5000
 
   private val client = HttpClient
     .newBuilder()
@@ -49,9 +75,7 @@ object UpdateInstitutions {
             Map(
               "query_string" -> Map(
                 "default_operator" -> "AND",
-                "fields" -> List(
-                  "provider.name"
-                ),
+                "fields" -> List("provider.name"),
                 "lenient" -> true,
                 "query" -> hubName.replaceAll("/", "\\\\/")
               )
@@ -68,19 +92,7 @@ object UpdateInstitutions {
         )
       )
     )
-    Serialization.write(query)(getFormats)
-  }
-
-  private def execute(request: HttpRequest): String = {
-    val response = client.send(request, BodyHandlers.ofString())
-    if (response.statusCode() == 200) {
-      response.body()
-    } else {
-      val msg = s"Unsuccessful request: ${request.uri().toString}\n" +
-        s"Code: ${response.statusCode()}\n" +
-        s"Message: ${response.body()}\n"
-      throw new RuntimeException(msg)
-    }
+    Serialization.write(query)
   }
 
   def getHubNamesQuery: String = {
@@ -97,20 +109,21 @@ object UpdateInstitutions {
         )
       )
     )
-    Serialization.write(query)(getFormats)
+    Serialization.write(query)
   }
+
+  def buildPostRequest(query: String): HttpRequest = HttpRequest
+    .newBuilder()
+    .method("POST", BodyPublishers.ofString(query))
+    .header("Content-Type", "application/json")
+    .uri(new URI(ELASTICSEARCH_ENDPOINT))
+    .build()
 
   def getHubNames: Seq[String] = {
 
     val query = getHubNamesQuery
-    val request = HttpRequest
-      .newBuilder()
-      .method("POST", BodyPublishers.ofString(query))
-      .header("Content-Type", "application/json")
-      .uri(new URI(ELASTICSEARCH_ENDPOINT))
-      .build()
-    val response = execute(request)
-
+    val request = buildPostRequest(query)
+    val response = HttpUtils.execute(request)
     val hubsJson = parse(response)
 
     val hubsNames =
@@ -119,7 +132,7 @@ object UpdateInstitutions {
           hubsJson \ "aggregations" \ "provider.name" \ "buckets" \ "key"
       ) yield term
 
-    if (hubsNames.length >= 4999)
+    if (hubsNames.length >= MAX_FACET_BUCKETS - 1)
       throw new RuntimeException(
         f"Warning: ${hubsNames.length} hubs found, some may be missing!"
       )
@@ -127,23 +140,17 @@ object UpdateInstitutions {
     hubsNames
   }
 
-  def getContributorNames(hubName: String, apiKey: String): Seq[String] = {
+  def getContributorNames(hubName: String): Seq[String] = {
     val query = getContributorNamesQuery(hubName)
-    val request = HttpRequest
-      .newBuilder()
-      .method("POST", BodyPublishers.ofString(query))
-      .header("Content-Type", "application/json")
-      .uri(new URI(ELASTICSEARCH_ENDPOINT))
-      .build()
-    val response = execute(request)
+    val request = buildPostRequest(query)
+    val response = HttpUtils.execute(request)
     val contributorJson = parse(response)
     val contributorNames = for {
       JString(term) <-
         contributorJson \ "aggregations" \ "dataProvider.name" \ "buckets" \ "key"
     } yield term
 
-    if (contributorNames.length >= 4999) {
-      println(contributorNames)
+    if (contributorNames.length >= MAX_FACET_BUCKETS - 1) {
       throw new RuntimeException(
         s"Warning: ${contributorNames.length} contributors found for $hubName, some may be missing!"
       )
@@ -153,35 +160,24 @@ object UpdateInstitutions {
   }
 
   def main(args: Array[String]): Unit = {
-    val apiKey = args(0)
-    val outFile = args(1)
-
-    implicit val formats: Formats = getFormats
     val institutionsJson = parse(
       new File("src/main/resources/wiki/institutions_v2.json")
     )
-    val institutionsData = Map.from(
-      institutionsJson match {
+    val institutionsData = (institutionsJson match {
         case JObject(values) =>
-          values.map(value => (value._1 -> value._2.extract[Hub]))
+          values.map(value => value._1 -> value._2.extract[Hub])
         case _ =>
           throw new RuntimeException(
             "Can't understand existing institutions file."
           )
-      }
-    )
+      }).toMap
 
     val hubNames = getHubNames
 
     val newHubs = hubNames
       .map(hubName => {
-        val existingHub =
-          institutionsData.getOrElse(hubName, Hub(None, Map.empty))
-        hubName -> updatedHub(
-          hubName,
-          existingHub,
-          apiKey
-        )
+        val existingHub = institutionsData.getOrElse(hubName, Hub())
+        hubName -> updatedHub(hubName, existingHub)
       })
       .toMap
 
@@ -192,28 +188,22 @@ object UpdateInstitutions {
 
     // no backsliding
     for (hubName <- institutionsData.keys) {
-      assert(
-        withDroppedHubs.contains(hubName),
-        f"Missing hub: $hubName"
-      )
+      if (!withDroppedHubs.contains(hubName))
+        throw new RuntimeException(f"Missing hub: $hubName")
       val oldHub = institutionsData(hubName)
       val newHub = withDroppedHubs(hubName)
-      assert(
-        oldHub.Wikidata == newHub.Wikidata,
-        f"Wikidata mismatch for hub: $hubName"
-      )
+      if (oldHub.Wikidata != newHub.Wikidata)
+        throw new RuntimeException(f"Wikidata mismatch for hub: $hubName")
       for (institutionName <- oldHub.institutions.keys) {
-        assert(newHub.institutions.contains(institutionName), f"Missing institution: $institutionName in hub: $hubName")
+        if (!newHub.institutions.contains(institutionName))
+          throw new RuntimeException(f"Missing institution: $institutionName in hub: $hubName")
         val oldContributingInstitution = oldHub.institutions(institutionName)
         val newContributingInstitution = newHub.institutions(institutionName)
-        assert(
-          oldContributingInstitution.upload == newContributingInstitution.upload,
-          f"Upload flag mismatch for institution: $institutionName in hub: $hubName"
-        )
-        assert(
-          oldContributingInstitution.Wikidata == newContributingInstitution.Wikidata,
-          f"Wikidata mismatch for institution: $institutionName in hub: $hubName"
-        )
+        if (oldContributingInstitution.upload != newContributingInstitution.upload)
+          throw new RuntimeException(f"Upload flag mismatch for institution: $institutionName in hub: $hubName")
+        if (
+          oldContributingInstitution.Wikidata != newContributingInstitution.Wikidata)
+          throw new RuntimeException(f"Wikidata mismatch for institution: $institutionName in hub: $hubName")
       }
     }
 
@@ -227,30 +217,30 @@ object UpdateInstitutions {
       f"$contributorsCount contributors found. $newContributorsCount new contributors added."
     )
 
+    val outJson = map2jvalue(withDroppedHubs)
+    val outJsonString = pretty(outJson).getBytes(StandardCharsets.UTF_8)
 
     Files.write(
-      Paths.get(outFile),
-      Serialization.write(newHubs).getBytes(StandardCharsets.UTF_8)
+      Paths.get("src/main/resources/wiki/institutions_v2.json"),
+      outJsonString
     )
   }
 
-  def updatedHub(hubName: String, prevHub: Hub, apiKey: String): Hub = {
-    val contributorNames = getContributorNames(hubName, apiKey)
+  private def updatedHub(hubName: String, prevHub: Hub): Hub = {
+    val contributorNames = getContributorNames(hubName)
     val newContributors = contributorNames
       .map(name =>
-        name -> prevHub.institutions
-          .getOrElse(name, ContributingInstitution(None, None))
+        name -> prevHub.institutions.getOrElse(name, ContributingInstitution())
       )
       .toMap
 
     val oldContributors = prevHub.institutions.keys.flatMap(contributorName =>
       if (!contributorNames.contains(contributorName))
         Some(contributorName -> prevHub.institutions(contributorName))
-      else None
+      else
+        None
     )
 
-    prevHub.copy(
-      institutions = newContributors ++ oldContributors
-    )
+    prevHub.copy(institutions =  newContributors ++ oldContributors)
   }
 }
